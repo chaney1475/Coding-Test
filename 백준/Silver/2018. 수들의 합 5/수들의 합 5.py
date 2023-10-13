971740acc4ca8d618e46559ec1874432948c797c
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2023-10-13 15:56:38 +0900

    [Silver V] Title: 수들의 합 5, Time: 3156 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -1,18 +1,14 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-start_i, end_i, count, sum = 1, 1, 1, 1
+end_i, count, sum = 1, 1, 1
 
-while end_i != n:
+for start in range(1, n):
+    while sum < n:
+        end_i += 1
+        sum += end_i
     if sum == n:
-        end_i += 1
-        sum += end_i
         count += 1
-    elif sum < n:
-        end_i += 1
-        sum += end_i
-    else:
-        sum -= start_i
-        start_i += 1
+    sum -= start
 
 print(count)

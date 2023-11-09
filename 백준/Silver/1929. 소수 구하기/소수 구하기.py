a8458e7b5c8c8488485899fda514db0dac4d74b7
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2023-11-09 15:09:51 +0900

    [Silver III] Title: 소수 구하기, Time: 480 ms, Memory: 71636 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -10,7 +10,7 @@ A = [0] *(M + 1)
 for i in range(2, M+1):
     A[i] = i
 
-for i in range(2, M//2 + 1):
+for i in range(2, int(math.sqrt(M)) + 1):
     if A[i] == 0:
         continue
     for j in range(i+i, M+1, i):

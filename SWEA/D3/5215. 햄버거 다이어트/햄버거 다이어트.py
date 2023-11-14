87cec998a4c7d959fd7f3907fcb51faa36245653
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2023-11-14 20:39:37 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,664 ms, Memory: 80,208 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,35 +1,31 @@
-def dfs(index, sTaste, sKcal):
-    global maxTaste
-    
-    # 총 칼로리를 넘으면 리턴
-    if sKcal > L:
-        return
-        
-    # taste의 합이 제일큰 taste 합보다 크면 갱신
-    if maxTaste < sTaste:
-        maxTaste = sTaste
-        
-    # 햄버거 재료 데이터를 다 돌면 리턴
-    if index == N:
-        return
-        
-    # index 파라미터를 통해 taste, kcal 대입
-    taste, kcal = data[index]
-    
-    # 햄버거 재료 리스트에서 재료를 사용했을 때
-    dfs(index + 1, sTaste + taste, sKcal + kcal)
-    # 햄버거 재료 리스트에서 재료를 사용하지 않았을 때
-    dfs(index + 1, sTaste, sKcal)
+test_case = int(input())
 
-t = int(input())
-for tc in range(1, t+1):
-    # N: 햄버거 재료 수, L: 넘지 말아야 하는 칼로리 양
+
+def dfs(index, x, temp):
+    global sum
+
+    for j in range(index,N):
+        if not visited[j] and x >= material[j][1]:
+            visited[j] = True
+            dfs(j+1, x - material[j][1], temp + material[j][0]) #길이가 가능하면 값 빼고 dfs
+            visited[j] = False
+
+    if sum < temp:
+        sum = temp
+
+for i in range(test_case):
+
+    # N이 종류 l이 제한 길이
     N, L = map(int, input().split())
-    
-    # 햄버거와 칼로리 리스트 저장
-    data = [list(map(int, input().split())) for _ in range(N)]
-    
-    maxTaste = 0
-    dfs(0, 0, 0)
-    
-    print("#{} {}".format(tc, maxTaste))
+
+    visited = [False] * N
+
+    sum = 0
+    material = []
+    for j in range(N):
+        a, b = map(int, input().split())
+        material.append((a, b))
+
+    dfs(0, L, 0)
+
+    print(f"#{i+1} {sum}")

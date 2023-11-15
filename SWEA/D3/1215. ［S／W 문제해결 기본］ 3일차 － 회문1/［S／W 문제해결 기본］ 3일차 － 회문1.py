8d5ab39b130ae3e6be5631cc2e81784dc74bd521
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2023-11-15 20:55:58 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 156 ms, Memory: 58,780 KB -BaekjoonHub

---
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -0,0 +1,34 @@
+# 회문1
+def is_palindrome(A):
+    while len(A) > 1:
+        if A.pop() != A.pop(0):
+            return False
+    return True
+
+
+for test_case in range(1, 11):
+    answer = 0
+    N = int(input())
+
+    A = []
+    for j in range(8):
+        A.append([c for c in input()])
+
+    if N == 1:
+        print(f"#{test_case} 64")
+    else:
+        for j in range(8):
+            for k in range(0, 9 - N):
+                str = A[j][k:k + N]
+                if is_palindrome(str):
+                    answer += 1
+
+        for j in range(8):
+            for k in range(0, 9 - N):
+                str = []
+                for i in range(0,N):
+                    str.append(A[i+k][j])
+                if is_palindrome(str):
+                    answer += 1
+
+        print(f"#{test_case} {answer}")

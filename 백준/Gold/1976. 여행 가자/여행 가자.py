efb70027c853e7ceb9598606a0ef73efdefb6e47
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-01-23 19:53:57 +0900

    [Gold IV] Title: 여행 가자, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,49 +1,35 @@
 import sys
-
-sys.setrecursionlimit(10000)
-
 input = sys.stdin.readline
-N = int(input())
 
-A = []
-
-B = [x for x in range(N + 1)]
-
-M = int(input())
-
-for i in range(N):
-    A.append(list(map(int, input().split())))
-
-path = list(map(int, input().split()))
-
-
-def REP(x):
-    if B[x] == x:
-        return x
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x < y:
+        parents[y] = x
     else:
-        B[x] = REP(B[x])
-        return B[x]
+        parents[x] = y
+    
+def find(x):
+    if x != parents[x]:
+        parents[x] = find(parents[x])
+    return parents[x]
 
+# 유니온 파인드
+n,m = int(input()),int(input())
+parents = [i for i in range(n)]
+for i in range(n):
+    link = list(map(int,input().split()))
+    for j in range(n):
+        if link[j] == 1:
+            union(i,j)
 
-def UNION(a, b):
-    x = REP(a)
-    y = REP(b)
-    if x != y:
-        B[y] = x
-
-
-for i in range(N):
-    for j in range(N):
-        if A[i][j] == 1:
-            UNION(i, j)
-
-num = REP(B[path[0] - 1])
-
-answer = "YES"
-for i in path:
-    if num != REP(B[i - 1]):
-        answer = "NO"
+# 경로 체크
+parents = [-1] + parents
+path = list(map(int,input().split()))
+start = parents[path[0]]
+for i in range(1,m):
+    if parents[path[i]] != start:
+        print("NO")
         break
-print(answer)
-
-
+else:
+    print("YES")

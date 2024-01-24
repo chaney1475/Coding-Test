bb66942edea34c3de8267f57203123ad6bf0cd3f
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-01-24 14:00:10 +0900

    [Gold III] Title: 줄 세우기, Time: 4256 ms, Memory: 38580 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+N, M = map(int, input().split())
+
+A = [[] for _ in range(N + 1)]
+D = [0 for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    A[a].append(b)
+    D[b] += 1
+
+q = deque()
+answer = []
+for i in range(1, N + 1):
+    if D[i] == 0:
+        q.append(i)
+
+while (q):
+    now = q.popleft()
+    answer.append(now)
+    for a in A[now]:
+        D[a] -= 1
+        if D[a] == 0:
+            q.append(a)
+
+print(*answer)

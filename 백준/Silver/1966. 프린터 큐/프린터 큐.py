89ddbd615ce16e50b98a76f69c92a20784a76d15
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-01-29 17:10:14 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,24 +1,26 @@
 from collections import deque
 import sys
 
-t = int(input())
+input = sys.stdin.readline
 
-for i in range(t):
-    n, m = map(int, input().split())
-    queue = deque(list(map(int, sys.stdin.readline().split())))
-    count = 0
-    while queue:
-        best = max(queue)  #현재의 최댓값이 가장 먼저 배출되므로 최댓값을 저장
-        front = queue.popleft() # 큐의 front를 뽑았으므로
-        m -= 1 # 내 위치가 한 칸 당겨진다.
+batch = int(input())
 
-        if best == front: # 뽑은 숫자가 제일 큰 숫자일 때
-            count += 1 # 하나가 영원히 배출되므로 순번 하나 추가
-            if m < 0: # m이 0이라는 것은 뽑은 숫자가 내 숫자라는 뜻.
-                print(count)
+for _ in range(batch):
+    N, M = map(int, input().split())
+
+    dq = deque(list(map(int, input().split())))
+    cnt = 0
+    while dq:
+        best = max(dq)
+        i = dq.popleft()
+        if i == best:
+            cnt += 1
+            if M == 0:
                 break
+        else:
+            dq.append(i)
 
-        else:   # 뽑은 숫자가 제일 큰 숫자가 아니면
-            queue.append(front) # 제일 뒤로 밀려나게 됨
-            if m < 0 :  # 제일 앞에서 뽑히면
-                m = len(queue) - 1 # 제일 뒤로 이동
+        M -= 1
+        if M == -1:
+            M = len(dq) - 1
+    print(cnt)

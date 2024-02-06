03e3687289a4040a80b2e45ccfe9177a68be5b7b
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-06 16:37:35 +0900

    [Silver II] Title: 이항 계수 2, Time: 292 ms, Memory: 53660 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+MOD = 10007
+
+N, K = map(int,input().split())
+
+A = [[0] * (N+1) for _ in range(N+2)]
+
+for i in range(1,N+1):
+    A[i][i] = 1
+    A[i][1] = i
+    A[i][0] = 1
+
+for i in range(2,N+1):
+    for j in range(1,i):
+        A[i][j] = A[i-1][j] + A[i-1][j-1] 
+        A[i][j] = A[i][j] % MOD
+
+print(A[N][K])

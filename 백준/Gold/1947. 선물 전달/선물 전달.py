a8d142b417ede3c3c79160c80347537a15f0c1af
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-09 11:55:06 +0900

    [Gold III] Title: 선물 전달, Time: 444 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Gold/1947. 선물 전달/선물 전달.py
+++ 백준/Gold/1947. 선물 전달/선물 전달.py
@@ -1,22 +1,16 @@
 import sys
+
 sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
 N = int(input())
 MOD = 1000000000
 
-D = [0] * (N + 3)
-D[1] = 0
+D = [0] * (1000001)
 D[2] = 1
-D[3] = 2
 
 
-def dp(n):
-    if n < 2:
-        return 0
-    if D[n] == 0:
-        D[n] = (n - 1)*(dp(n - 1) + dp(n - 2))% MOD
-    return D[n]
+for i in range(3,N+1):
+    D[i] = (i - 1) * (D[i - 1] + D[i - 2]) % MOD
 
-
-print(dp(N))
+print(D[N])

b927509d05cae481b038004f9809cb3327129781
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-11 14:25:40 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -6,9 +6,7 @@ N = int(input())
 D = [0] * (90+1)
 D[1] = 1
 D[2] = 1
-D[3] = 2
-D[4] = 3
-for i in range(5,91):
-    D[i] = D[i-2]*2 + D[i-3]
+for i in range(3,91):
+    D[i] = D[i-1] +D[i-2]
 
 print(D[N])

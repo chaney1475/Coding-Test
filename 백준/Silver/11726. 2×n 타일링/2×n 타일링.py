25ab9948c4c442984269ebf986a956f42410f6ac
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-11 15:24:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 228 ms, Memory: 49808 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -11,14 +11,9 @@ for i in range(1, N + 1):
     A[i][1] = i
     A[i][0] = 1
 
-
-def dp(a, b):
-    if a == 0:
-        return 0
-    if A[a][b] == 0:
-        A[a][b] = dp(a - 1, b - 1) + dp(a - 1, b) % MOD
-    return A[a][b]
-
+for i in range(3, N+1):
+    for j in range(2, i):
+        A[i][j] = A[i-1][j-1] + A[i-1][j]% MOD
 
 answer = 1
 k = 0
@@ -26,6 +21,6 @@ for i in range(N - 1, 0, -1):
     k += 1
     if k > N // 2:
         break
-    answer += dp(i, k)
+    answer += A[i][k]
 
 print(answer % MOD)

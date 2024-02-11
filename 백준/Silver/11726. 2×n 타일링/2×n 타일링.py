c531b6f32cbcfcd9ce957e80569877383cfd8c3f
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-11 15:38:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,26 +1,12 @@
 import sys
-
 input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
+dp =[0] *1001
+dp[1] = 1
+dp[2] = 2
 
-A = [[0] * (i + 1) for i in range(N + 1)]
-MOD = 10007
-for i in range(1, N + 1):
-    A[i][i] = 1
-    A[i][1] = i
-    A[i][0] = 1
-
-for i in range(3, N+1):
-    for j in range(2, i):
-        A[i][j] = A[i-1][j-1] + A[i-1][j]% MOD
-
-answer = 1
-k = 0
-for i in range(N - 1, 0, -1):
-    k += 1
-    if k > N // 2:
-        break
-    answer += A[i][k]
-
-print(answer % MOD)
+for i in range(3,n+1): 
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    
+print(dp[n])

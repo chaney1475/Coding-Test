269838f938b8e1f7741a1c0e8f7783a726802a43
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-15 14:57:07 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver I/1149. RGB거리/RGB거리.py
+++ 백준/Silver I/1149. RGB거리/RGB거리.py
@@ -13,12 +13,9 @@ for _ in range(N):
 for i in range(3):
     D[1][i] = cost[0][i]
 
-for i in range(3):
-    for j in range(3):
-        if i == j:
-            continue
-        #cost는 무조건 -1해서 계산
-        D[2][i] = min(D[2][i], D[1][j] + cost[1][i])
+D[2][0] = min(D[1][1] + cost[1][0] ,D[1][2] + cost[1][0])
+D[2][1] = min(D[1][0] + cost[1][1] ,D[1][2] + cost[1][1])
+D[2][2] = min(D[1][1] + cost[1][2] ,D[1][0] + cost[1][2])
 
 #k번째 집부터 찾기
 k = 3

b04e8f701824b7c72439299dcfdffb1b74a00c3b
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-16 16:05:42 +0900

    [Silver I] Title: 카드 구매하기, Time: 216 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -2,15 +2,12 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-P = list(map(int,input().split()))
+P = [0] + list(map(int,input().split()))
 
 dp = [0] * (1000+1)
 
-for i in range(N):
-    dp[i+1] = P[i]
-
-for i in range(2, N+1):
-    for j in range(1,i):
-        dp[i] = max(dp[i], dp[j] + P[i-j-1])
+for i in range(1, N+1):
+    for j in range(1,i+1):
+        dp[i] = max(dp[i], dp[i-j] + P[j])
 
 print(dp[N])

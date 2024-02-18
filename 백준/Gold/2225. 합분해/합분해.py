b977ec5393d3ac20691226971a82082286e45b88
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-18 16:11:07 +0900

    [Gold V] Title: 합분해, Time: 60 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -3,15 +3,11 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 MOD = 1000000000
-D = [[0] * (201) for _ in range(201)]
+D = [[1] * (201) for _ in range(201)]
 
-for i in range(201):
-    D[i][1] = 1
-    D[0][i] = 1
 
 for i in range(1,201):
     for j in range(2, 201):
         D[i][j] = (D[i-1][j] + D[i][j-1]) % MOD
-
 
 print(D[N][K])

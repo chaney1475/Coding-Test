7da96387d1d061438e93ed9ea9c91c17c2bffd0d
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-27 15:54:26 +0900

    [Silver I] Title: 스타트와 링크, Time: 5568 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,54 +1,29 @@
-#스타트와 링크
 import sys
 input = sys.stdin.readline
-
 N = int(input())
+board = [list(map(int,input().split())) for _ in range(N)]
+visited = [False for _ in range(N)]
+INF = 2147000000
+res = INF
 
-A = []
-sum_total = 0
-for _ in range(N):
-    a = list(map(int, input().split()))
-    A.append(a)
-    sum_total += sum(a)
-
-visited = [False] * 20
-
-
-min_total = 20000
-
-def check():
-    global min_total
-
-    total1 = 0
-    total2 = 0
-    flag = False
-    for i in range(N):
-        for j in range(N):
-            if i == j:
-                continue
-            if visited[i] and visited[j]:
-                flag = True
-                total1 += A[i][j]
-            if not visited[i] and not visited[j]:
-                total2 += A[i][j]
-    if not flag:
+def DFS(L,idx):
+    global res
+    if L == N//2:
+        A = 0
+        B = 0
+        for i in range(N):
+            for j in range(N):
+                if visited[i] and visited[j]:
+                    A += board[i][j]
+                elif not visited[i] and not visited[j]:
+                    B +=board[i][j]
+        res = min(res, abs(A-B))
         return
-    if abs(total1-total2) < min_total:
-        min_total = abs(total1 - total2)
-
-
-def dfs(n, cnt):
-    if cnt == N//2:
-        check()
-        return
-    if n == N:
-        return
-    dfs(n+1,cnt)
-    visited[n] = True
-    dfs(n+1, cnt+1)
-    visited[n] = False
-
-dfs(0,0)
-
-
-print(min_total)
+    for i in range(idx,N):
+        if not visited[i]:
+            visited[i] = True
+            DFS(L+1,i+1)
+            visited[i] = False
+            
+DFS(0,0)
+print(res)

1f0fa8241f68f5ab4b7a8ede6a7d8150bd6ad1d3
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-02-28 16:27:23 +0900

    [Gold II] Title: 2048 (Easy), Time: 188 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -41,26 +41,7 @@ def dfs(cnt, board):
     # 왼쪽으로 밀기
     for i in range(N):
         c = make_newL(copy_board[i])
-        new_l = []
-        if len(c) == 1:
-            new_l = c
-        j = 1
-        while j < len(c):
-            if c[j - 1] == c[j]:
-                new_l.append(c[j] * 2)
-                if j == len(c) - 2:
-                    new_l.append(c[j + 1])
-                j += 2
-
-            else:
-                new_l.append(c[j - 1])
-                if j == len(c) - 1:
-                    new_l.append(c[j])
-                j += 1
-        k = len(new_l)
-        for _ in range(N - k):
-            new_l.append(0)
-
+        new_l = push(c)
         for h in range(N):
             copy_board[i][h] = new_l[h]
 
@@ -69,30 +50,8 @@ def dfs(cnt, board):
     copy_board = [[i for i in board[j]] for j in range(N)]
     # 오른쪽으로 밀기
     for i in range(N):
-        new_l = []
         c = make_newL(copy_board[i])
-
-        if len(c) == 1:
-            new_l = c
-
-        j = len(c) - 2
-        while j > -1:
-            if c[j] == c[j + 1]:
-                new_l.append(c[j] * 2)
-                if j == 1:
-                    new_l.append(c[0])
-                j -= 2
-
-            else:
-                new_l.append(c[j + 1])
-                if j == 0:
-                    new_l.append(c[j])
-                j -= 1
-
-        k = len(new_l)
-        for _ in range(N - k):
-            new_l.append(0)
-        new_l = new_l[::-1]
+        new_l = push(c[::-1])[::-1]
         for h in range(N):
             copy_board[i][h] = new_l[h]
 
@@ -100,66 +59,49 @@ def dfs(cnt, board):
 
     copy_board = [[i for i in board[j]] for j in range(N)]
     # 위로 밀기
+
     for i in range(N):
-        new_l = []
         c = make_CnewL(copy_board, i)
-        if len(c) == 1:
-            new_l = c
-        j = 1
-        while j < len(c):
-            if c[j - 1] == c[j]:
-                new_l.append(c[j] * 2)
-                if j == len(c) - 2:
-                    new_l.append(c[j + 1])
-                j += 2
-
-            else:
-                new_l.append(c[j - 1])
-                if j == len(c) - 1:
-                    new_l.append(c[j])
-                j += 1
-        k = len(new_l)
-        for _ in range(N - k):
-            new_l.append(0)
-
+        new_l = push(c)
         for h in range(N):
             copy_board[h][i] = new_l[h]
-
     dfs(cnt + 1, copy_board)
 
     copy_board = [[i for i in board[j]] for j in range(N)]
 
     # 아래로 밀기
     for i in range(N):
-        new_l = []
         c = make_CnewL(copy_board,i)
+        new_l = push(c[::-1])[::-1]
 
-        if len(c) == 1:
-            new_l = c
-
-        j = len(c) - 2
-        while j > -1:
-            if c[j] == c[j + 1]:
-                new_l.append(c[j] * 2)
-                if j == 1:
-                    new_l.append(c[0])
-                j -= 2
-
-            else:
-                new_l.append(c[j + 1])
-                if j == 0:
-                    new_l.append(c[j])
-                j -= 1
-
-        k = len(new_l)
-        for _ in range(N - k):
-            new_l.append(0)
-        new_l = new_l[::-1]
         for h in range(N):
             copy_board[h][i] = new_l[h]
 
     dfs(cnt + 1, copy_board)
-    copy_board = [[i for i in board[j]] for j in range(N)]
+
+
+def push(c):
+    new_l = []
+    if len(c) == 1:
+        new_l = c
+    j = 1
+    while j < len(c):
+        if c[j - 1] == c[j]:
+            new_l.append(c[j] * 2)
+            if j == len(c) - 2:
+                new_l.append(c[j + 1])
+            j += 2
+
+        else:
+            new_l.append(c[j - 1])
+            if j == len(c) - 1:
+                new_l.append(c[j])
+            j += 1
+    k = len(new_l)
+
+    for _ in range(N - k):
+        new_l.append(0)
+    return new_l
 
 
 if N == 1:

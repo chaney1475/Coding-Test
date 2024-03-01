c09c0ed4d73c0d6dde1b1183829fd59334c74ffa
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-03-01 16:35:35 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1464 ms, Memory: 87412 KB -BaekjoonHub

---
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+S = int(input())
+
+work = []
+total = 0
+
+for i in range(S):
+    value = list(map(int, input().split()))
+    if len(value) == 3:
+        a, b, c = value
+        work.append([b,c])
+    if len(work) == 0:
+        continue
+    work[-1][1] -= 1
+    if work[-1][1] == 0:
+        total += work[-1][0]
+        work.pop(-1)
+
+print(total)

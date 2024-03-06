980340858290e9e8c39c99b0e189227aa14df477
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-03-06 12:38:25 +0900

    [Silver I] Title: 포도주 시식, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+#잔 갯수
+N = int(input())
+
+w = []
+
+for _ in range(N):
+    w.append(int(input()))
+
+
+dp = [0] * (N+1)
+dp[0] = w[0]
+if N > 1:
+    dp[1] = w[0] + w[1]
+if N > 2:
+    dp[2] = max(w[0] + w[2], w[1] + w[2],w[0]+w[1])
+
+for i in range(3, N):
+    dp[i] = max(dp[i-3] + w[i-1]+w[i],
+                dp[i-2] + w[i],
+                dp[i-1])
+
+print(max(dp[N-1],dp[N-2]))
+

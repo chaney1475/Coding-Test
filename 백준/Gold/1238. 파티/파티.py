ea6e09211425145bc8eec309decb12418b4579c5
Chaeyeon <86292444+chaney1475@users.noreply.github.com>
2024-03-15 15:31:41 +0900

    [Gold III] Title: 파티, Time: 56 ms, Memory: 34240 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,29 +1,42 @@
+import sys
 import heapq
-
-def dijkstra(s):
-    D = [float('inf')] * (N+1)
-    D[s] = 0
-    q = []
-    heapq.heappush(q, (0, s))
-    while q:
-        dist, now = heapq.heappop(q)
-        if D[now] >= dist:
-            for v, val in city[now]:
-                if dist + val < D[v]:
-                    D[v] = dist + val
-                    heapq.heappush(q, (dist + val, v))
-    return D
+input = sys.stdin.readline
 
 N, M, X = map(int, input().split())
-city = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b, t = map(int, input().split())
-    city[a].append([b, t])
-ans = dijkstra(X)
-ans[0] = 0
-for i in range(1, N+1):
-    if i != X:
-        res = dijkstra(i)
-        ans[i] += res[X]
+INF = sys.maxsize
 
-print(max(ans))
+A = [[] for _ in range(N+1)]
+reverse_A = [[] for _ in range(N+1)]
+
+d = [INF] * (N+1)
+d2 = [INF] * (N+1)
+
+for _ in range(M):
+    a,b, w = map(int, input().split())
+    A[a].append((b, w))
+    reverse_A[b].append((a,w))
+
+
+def dijkstra(g, distance, start):
+    q = []
+    heapq.heappush(q,(0, start))
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[now]: continue
+        for j in g[now]:
+            cost = j[1] + dist
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+                heapq.heappush(q, (cost, j[0]))
+
+
+dijkstra(A, d, X)
+dijkstra(reverse_A, d2, X)
+
+d[0] = 0
+d2[0] = 0
+
+d = [d[i] + d2[i] for i in range(N+1)]
+d[X] = 0
+print(max(d))
+

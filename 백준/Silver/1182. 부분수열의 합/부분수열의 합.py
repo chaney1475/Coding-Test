2657037f417b4049bb18ca9131d48eb1aff897ee
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-04-11 12:42:37 +0900

    [Silver II] Title: 부분수열의 합, Time: 296 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,19 +1,25 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000000)
-
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-A.sort()
-
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
 cnt = 0
 
-def dfs(last, temp):
+def subset_sum(idx, sub_sum):
     global cnt
-    if last != -1 and temp == M:
-        cnt += 1
-    for i in range(last+1,len(A)):
-        dfs(i,temp + A[i])
 
-dfs(-1,0)
+    if idx >= n:
+        return
+
+    sub_sum += arr[idx]
+
+    if sub_sum == s:
+        cnt += 1
+    
+    # 현재 arr[idx]를 선택한 경우의 가지
+    subset_sum(idx+1, sub_sum)
+
+    # 현재 arr[idx]를 선택하지 않은 경우의 가지
+    subset_sum(idx+1, sub_sum - arr[idx])
+
+subset_sum(0, 0)
 print(cnt)

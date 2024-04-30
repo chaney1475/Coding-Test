3596be75c73b7ea436129cdf3098c14d03db45de
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-04-30 17:07:30 +0900

    [Silver I] Title: 스타트링크, Time: 824 ms, Memory: 71020 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+#전체 F층 S에서 시작 G에서 도착
+F,S,G,U,D = map(int,input().split())
+dp = [sys.maxsize] * (F+1)
+
+def bfs(start):
+    q = deque()
+    q.append([start,0])
+    dp[start] = 0
+    while q:
+        now, cnt = q.popleft()
+        if now + U <= F and dp[now + U] > cnt + 1:
+            dp[now + U] = cnt + 1
+            q.append([now+U,cnt+1])
+        if now - D >= 1 and dp[now - D] > cnt + 1:
+            dp[now - D] = cnt +1
+            q.append([now-D,cnt+1])
+
+bfs(S)
+if dp[G] == sys.maxsize:
+    print("use the stairs")
+else:
+    print(dp[G])

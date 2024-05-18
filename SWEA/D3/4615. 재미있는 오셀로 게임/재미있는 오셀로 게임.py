d31c13c27c8cbb5599750bd35c8b2b38f7eca28e
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-05-18 11:00:41 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 202 ms, Memory: 50,092 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
@@ -1,72 +1,62 @@
-#오셀로
-
-t = int(input())
+T = int(input())
 
 
-def check_my_color(x, y, i, j, color):
-    while 1 <= x + i <= N and 1 <= y + j <= N:
-        x += i
-        y += j
-        if A[x][y] == color:
-            return True
-        elif A[x][y] == 0:
-            return False
-    return False
+dx = [-1,-1,-1,0,0, 1,1,1]
+dy = [-1, 0, 1,-1,1,-1,0,1]
 
 
-def change(x, y, i, j, color, other):
-    while 1 <= x + i <= N and 1 <= y + j <= N:
-        x += i
-        y += j
-        if A[x][y] == other:
-            A[x][y] = color
-        else:
-            return
+def play():
+    for x, y, color in put:
+
+        A[x][y] = color
+        op = 1 if color == 2 else 2
+
+        for i in range(8):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            #다음 돌확인
+            if nx < 0 or nx >= N and ny < 0 or ny >= N:
+                continue
+            temp = []
+            while 0 <= nx < N and 0 <= ny < N and A[nx][ny] == op:
+                temp.append([nx,ny])
+                nx = dx[i] + nx
+                ny = dy[i] + ny
+            #반복문에서 나가고 다음돌이 현재 색과 일치하는지
+            if 0 <= nx < N and 0 <= ny < N and A[nx][ny] == color:
+                for tx, ty in temp:
+                    A[tx][ty] = color
+
+def cnt_color():
+    black = 0
+    white = 0
+
+    for i in range(N):
+        for j in range(N):
+            if A[i][j] == 1:
+                black += 1
+            elif A[i][j] == 2:
+                white += 1
+
+    return black, white
 
 
-def check(x, y, color):
 
-    grid = [0, -1, 1]
-
-    if color == 2:
-        other = 1
-    elif color == 1:
-        other = 2
-
-    flag = 0
-    for i in grid:
-        for j in grid:
-            if not (i == 0 and j == 0):
-                if 1 <= x + i <= N and 1 <= y + j <= N and A[x + i][y + j] == other:
-                    if check_my_color(x, y, i, j, color):
-                        change(x, y, i, j, color, other)
-                        flag = 1
-    if flag == 1:
-        return True
-
-
-for test_case in range(1,t+1):
-
-    #변의 길이 , 횟수
+for t in range(1,T+1):
     N, M = map(int, input().split())
 
-    A = [[0] * (N+1) for _ in range(N+1)]
-    n = N // 2
+    A = [[0] * N for _ in range(N)] #보드
+    A[N//2-1][N//2-1] = 2
+    A[N//2][N//2] = 2
+    A[N//2-1][N//2] = 1
+    A[N//2][N//2-1] = 1
 
-    A[n][n], A[n][n+1], A[n+1][n], A[n+1][n+1] = 2,1,1,2
-
+    put = [] #돌놓기
     for _ in range(M):
-        a, b, color = map(int, input().split())
-        if A[a][b] == 0 and check(a, b, color):
-            A[a][b] = color
+        y,x,c = map(int, input().split())
+        #1 흑돌, 2 백돌
+        put.append([x-1,y-1,c])
+    play()
+    a,b = cnt_color()
+    print(f"#{t} {a} {b}")
 
-    w = 0
-    b = 0
-
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            if A[i][j] == 2:
-                w += 1
-            elif A[i][j] == 1:
-                b += 1
-    print(f"#{test_case} {b} {w}")

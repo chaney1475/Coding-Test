e2e05ff11fa0556dddb3dcfd3a195fa2da99f071
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-07-26 12:14:47 +0900

    [Silver I] Title: 그림, Time: 1596 ms, Memory: 212912 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static int n,m;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int count = 0;
+    static int[][] pos = {{0,-1},{-1,0},{0,1},{1,0}};
+
+    static void dfs(int x, int y){
+        visited[x][y] = true;
+        count++;
+
+        for (int i = 0; i < 4; i++){
+            int nx = x + pos[i][0];
+            int ny = y + pos[i][1];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m || graph[nx][ny] == 0 || visited[nx][ny]){
+                continue;
+            }
+
+            dfs(nx, ny);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        boolean zero = true;
+        List<Integer> answer = new ArrayList<>();
+        graph = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int i = 0; i< n; i++){
+            for (int j = 0; j < m; j++){
+                graph[i][j] = sc.nextInt();
+                if(graph[i][j] == 1){
+                    zero = false;
+                }
+            }
+        }
+
+        if(zero){
+            System.out.println("0");
+            System.out.println("0");
+            return;
+        }
+
+        for (int i = 0; i< n ; i ++){
+            for (int j = 0; j < m; j++){
+                count = 0;
+                if (!visited[i][j] && graph[i][j] == 1 ){
+                    dfs(i,j);
+                    answer.add(count);
+                }
+            }
+        }
+        Collections.sort(answer);
+        System.out.println(answer.size());
+        System.out.println(answer.get(answer.size() - 1));
+    }
+}

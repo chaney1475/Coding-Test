ec54ae86b3e1a31ddd84066230b4e7830f974266
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-08-14 12:32:39 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1536 ms, Memory: 75860 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,29 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main { // 구간 합 구하기 4
+	
+	static int N;
+	static int M;
+	static int[] dp;
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        int suNo = Integer.parseInt(st.nextToken());
-        int quizNo = Integer.parseInt(st.nextToken());
-
-        long[]S = new long[suNo +1];
-        st = new StringTokenizer(bf.readLine());
-        for(int i = 1; i <= suNo; i++){
-            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
-        }
-        for(int q = 0; q <quizNo; q++){
-            st = new StringTokenizer(bf.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            System.out.println(S[j]-S[i-1]);
-        }
-
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken()); // N개의 숫자
+		M = Integer.parseInt(st.nextToken()); // M 번 물어볼것
+		
+		st = new StringTokenizer(br.readLine());
+		
+		dp = new int[N];
+				
+		int sum = 0;
+		for(int i = 0; i < N; i++) {
+			sum += Integer.parseInt(st.nextToken());
+			dp[i] = sum;
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int answer;
+			int k = Integer.parseInt(st.nextToken());
+			int kk = Integer.parseInt(st.nextToken());
+			if (k == 1) {
+				answer = dp[kk-1];
+			}
+			else
+				answer = dp[kk-1] - dp[k-2];
+			System.out.println(answer);
+		}
+		
+	}
+	
 }

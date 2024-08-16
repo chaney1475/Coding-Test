72b07139aff9c2a5236fd5168299fc620b886bad
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-08-16 13:43:14 +0900

    [Gold V] Title: 신기한 소수, Time: 92 ms, Memory: 13240 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -5,23 +5,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int[] answer;
 	static int N;
 
 	static boolean isPrime(int n, int i) {
-        // 기본 케이스: 2보다 작은 수는 소수가 아님
         if (n <= 1) {
             return false;
         }
-        // i가 n의 제곱근보다 크면 n은 소수임
         if (i * i > n) {
             return true;
         }
-        // n이 i로 나누어 떨어지면 소수가 아님
         if (n % i == 0) {
             return false;
         }
-        // 다음 숫자로 재귀 호출
         return isPrime(n, i + 1);
     }
 
@@ -31,11 +26,9 @@ public class Main {
 
 		N = sc.nextInt();
 
-		answer = new int[N];
 
 		for (int i = 2; i < 8; i++) {
 			if (isPrime(i,2)) {
-				answer[0] = i;
 				makeNum(1, i, bw);
 			}
 		}
@@ -51,10 +44,9 @@ public class Main {
 			return;
 		}
 
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 10; i+=2) {
 			int newNum = num * 10 + i;
 			if (isPrime(newNum,2)) {
-				answer[index] = i;
 				makeNum(index + 1, newNum, bw);
 			}
 		}

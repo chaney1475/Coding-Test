59ca8f3a82e20b3d66b3e3c71793b3b3d5383c1e
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-08-25 22:18:54 +0900

    [Silver IV] Title: ATM, Time: 192 ms, Memory: 18628 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,30 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            answer += nums[i];
+            nums[i] = answer;
+        }
+        System.out.println(Arrays.stream(nums).sum());
+    }
+
+
+}

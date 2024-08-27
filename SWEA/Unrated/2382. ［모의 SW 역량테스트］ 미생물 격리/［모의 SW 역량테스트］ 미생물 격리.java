3556b6beeb81b6aecc565cde21e93725e5f2a8bf
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-08-27 21:46:24 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 1,096 ms, Memory: 115,312 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -1,168 +1,92 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
-	// 미생물 군집
-	static int[] dx = { -1, 1, 0, 0 }; // 상하좌우 (-1을 해야함)
-	static int[] dy = { 0, 0, -1, 1 };
+    static int[] dx = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dy = {0, 0, -1, 1};
 
-	static int N;
-	static int K;
-	static int M;
+    static int N, K, M;
 
-	static int[][][] grid;
+    static class Microbe implements Comparable<Microbe> {
+        int x, y, count, dir;
 
-	public static void main(String[] args) throws IOException {
+        Microbe(int x, int y, int count, int dir) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+            this.dir = dir;
+        }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        void move() {
+            this.x += dx[dir];
+            this.y += dy[dir];
 
-		StringTokenizer st;
+            if (x == 0 || x == N - 1 || y == 0 || y == N - 1) {
+                count /= 2; // 미생물 수 반감
+                dir = (dir % 2 == 0) ? dir + 1 : dir - 1; // 방향 반전
+            }
+        }
 
-		int T = Integer.parseInt(br.readLine());
+        @Override
+        public int compareTo(Microbe other) {
+            return other.count - this.count; // 내림차순 정렬 (큰 수부터)
+        }
+    }
 
-		for (int t = 1; t <= T; t++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-			st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-			N = Integer.parseInt(st.nextToken()); // 한 변에 잇는 셀의 수
-			M = Integer.parseInt(st.nextToken()); // 격리 시간 - 몇번의 미생물 이동이 필요한지
-			K = Integer.parseInt(st.nextToken()); // 미생물 군집의 수
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
 
-			grid = new int[N][N][2];
+            List<Microbe> microbes = new ArrayList<>();
 
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                int count = Integer.parseInt(st.nextToken());
+                int dir = Integer.parseInt(st.nextToken()) - 1;
 
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
+                microbes.add(new Microbe(x, y, count, dir));
+            }
 
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
+            // M 시간 동안 미생물 이동
+            for (int m = 0; m < M; m++) {
+                Map<String, PriorityQueue<Microbe>> map = new HashMap<>();
 
-				// 해당 위치의 미생물 수 [1] 방향 [0]
-				int microCnt = Integer.parseInt(st.nextToken());
-				int dir = Integer.parseInt(st.nextToken()) - 1;
+                for (Microbe microbe : microbes) {
+                    microbe.move();
 
-				grid[a][b][1] = microCnt;
-				grid[a][b][0] = dir;
-				
-				
+                    if (microbe.count > 0) {
+                        String key = microbe.x + "," + microbe.y;
+                        map.putIfAbsent(key, new PriorityQueue<>());
+                        map.get(key).add(microbe);
+                    }
+                }
 
-			}
+                // map을 통해 microbes 리스트 갱신
+                microbes.clear();
+                for (PriorityQueue<Microbe> pq : map.values()) {
+                    Microbe merged = pq.poll(); // 가장 큰 군집을 꺼냄
+                    while (!pq.isEmpty()) {
+                        Microbe next = pq.poll();
+                        merged.count += next.count; // 병합
+                    }
+                    microbes.add(merged); // 병합된 군집을 추가
+                }
+            }
 
-//			System.out.println("초기 군집");
-//			for (int i = 0; i < N; i++) {
-//				for (int j = 0; j < N; j++) {
-//					System.out.print(Arrays.toString(grid[i][j]) + " ");
-//				}
-//				System.out.println();
-//			}
-
-//			System.out.println();
-			// M 시간 만큼 미생물들을 이동 시키자
-			// M만큼 반복
-			int[][][] temp;
-			for (int m = 0; m < M; m++) {
-				// 새로운 군집
-				temp = new int[N][N][2];
-				List<Integer[]>[][] mircoList = new ArrayList[N][N];
-				
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						mircoList[i][j] = new ArrayList<>();
-					}
-				}
-				
-				
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						if (grid[i][j][1] != 0) { // 미생물이 있다는 것!
-
-							int dir = grid[i][j][0];
-
-							int nx = i + dx[dir];
-							int ny = j + dy[dir];
-
-							// 끝에 도달한 경우
-
-							if (nx == 0 || nx == N - 1 || ny == 0 || ny == N - 1) {
-								int microNums = grid[i][j][1] / 2;
-								if (microNums == 0) {
-									continue;
-								}
-								int newDir = -1;
-								
-								switch (dir) {
-								case 0:
-									newDir = 1;
-									break;
-								case 1:
-									newDir = 0;
-									break;
-								case 2:
-									newDir = 3;
-									break;
-								case 3:
-									newDir = 2;
-									break;
-								}
-								mircoList[nx][ny].add(new Integer[] {newDir, microNums});
-
-							} else {
-								mircoList[nx][ny].add(new Integer[] { grid[i][j][0], grid[i][j][1]});
-							}
-						}
-					}
-				}
-				
-				for (int i = 0; i < N; i ++) {
-					for (int j = 0; j < N; j++) {
-						if (mircoList[i][j].size() == 1) {
-							temp[i][j][0] = mircoList[i][j].get(0)[0];
-							temp[i][j][1] = mircoList[i][j].get(0)[1];
-						}
-						else if (mircoList[i][j].size() > 1){
-							int biggest = 0;
-							int dir = -1;
-							
-							for (int k = 0; k < mircoList[i][j].size(); k ++) {
-								if (biggest < mircoList[i][j].get(k)[1]) {
-									biggest = mircoList[i][j].get(k)[1];
-									dir = mircoList[i][j].get(k)[0];
-								}
-								temp[i][j][1] += mircoList[i][j].get(k)[1];
-							}
-							
-							temp[i][j][0] = dir;
-							
-						}
-					}
-				}
-				
-				grid = temp;
-
-
-			}
-
-			int answer = 0;
-
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					answer += grid[i][j][1];
-				}
-			}
-
-			System.out.printf("#%d %d\n", t, answer);
-
-		}
-
-	}
-
+            int answer = microbes.stream().mapToInt(m -> m.count).sum();
+            System.out.printf("#%d %d\n", t, answer);
+        }
+    }
 }

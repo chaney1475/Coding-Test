42e4509555c86823c4e774c447461a14cfca85ab
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-09-04 14:00:36 +0900

    [D3] Title: 햄버거 다이어트, Time: 165 ms, Memory: 32,236 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,60 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder res = new StringBuilder();
-    static int N, list[];
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        int TC = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= TC; tc++) {
-            int ans = 0;
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            int L = Integer.parseInt(st.nextToken());
-            int[][] matrix = new int[N][2];
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                matrix[i][0] = Integer.parseInt(st.nextToken());
-                matrix[i][1] = Integer.parseInt(st.nextToken());
-            }
-            
-            for (int i = 1; i <= N; i++) {
-                list = new int[N];
-                Arrays.fill(list, N - i, N, 1);
-                
-                while(true) {
-                    int val = 0;
-                    int cal = 0;
-                    
-                    for(int j = 0; j < N; j++) {
-                        val += list[j] * matrix[j][0];
-                        cal += list[j] * matrix[j][1];
-                    }
-                    
-                    if(cal <= L) ans = Math.max(ans, val);
-                    if(!np()) break;
-                }
-            }
-            
-            res.append(String.format("#%d %d\n", tc, ans));
-        }
-        System.out.println(res.toString());
-    }
-    
-    static boolean np() {
-        int pivot = N - 2;
-        while(pivot >= 0 && list[pivot] >= list[pivot + 1]) pivot--;
-        if(pivot < 0) return false;
-        int pointer = N - 1;
-        while(list[pointer] == 0) pointer--;
-        list[pivot] = 1;
-        list[pointer] = 0;
-        for (int i = 1; i <= (N - 1 - pivot) / 2; i++) {
-            int temp = list[pivot + i];
-            list[pivot + i] = list[N - i];
-            list[N - i] = temp;
-        }
-        return true;
-    }
+	static int dp[][];
+	
+	
+	static int N;
+	static int L;
+	
+	public static void main(String[] args) throws IOException {
+		// 햄다 - dp
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			
+
+			dp = new int[N+1][L + 1];
+			
+			for (int i = 1; i <= N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int taste = Integer.parseInt(st.nextToken());
+				int cal = Integer.parseInt(st.nextToken());
+				for (int j = 0; j < cal; j++) {
+					dp[i][j] = dp[i-1][j];
+				}
+				for (int j = cal; j <= L; j++) {
+					dp[i][j] = Math.max(dp[i - 1][j-cal] + taste, dp[i-1][j]);
+				}
+			}
+			System.out.printf("#%d %d\n", t, dp[N][L]);
+			
+		}
+
+	}
+
 }

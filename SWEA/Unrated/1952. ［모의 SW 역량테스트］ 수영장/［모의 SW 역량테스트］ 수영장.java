406634c55fb854466fc39814a04f5e0220dd6637
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-09-04 10:26:41 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 130 ms, Memory: 20,136 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -3,8 +3,8 @@ import java.io.*;
 
 public class Solution {
     static int[] fee = new int[4]; 
-    static int[] date = new int[14];
     static int[] plan = new int[13];
+    static int[] dp = new int[13];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,37 +12,38 @@ public class Solution {
         
         int T = Integer.parseInt(br.readLine());
         for(int t = 1; t <= T; t++) {
+        	// 요금제 입력 받기
             st = new StringTokenizer(br.readLine());
-            for(int i = 2; i < 14; i++) date[i] = Integer.MAX_VALUE;
-            for(int i = 0; i < 4; i++) fee[i] = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            for(int i = 1; i < 13; i++) plan[i] = Integer.parseInt(st.nextToken());
             
-            //date[n]은 n전 날까지의 최소 비용
-            // 즉 n+1일 n일의 최소값임
+            for (int i = 0; i < 4; i++) {
+                fee[i] = Integer.parseInt(st.nextToken()); // 하루, 한 달, 세 달, 1년 요금 입력
+            }
 
-//            System.out.println(Arrays.toString(date));
-            for(int n = 0; n < 13; n++) {
-            	if(n+1 <= 13 && fee[0] * plan[n] + date[n] < date[n+1]) {
-                    date[n+1] = fee[0] * plan[n] + date[n];
-                }
-                //1달권
-                if(n+1 <= 13 && fee[1] + date[n] < date[n+1]) {
-                    date[n+1] = fee[1] + date[n];
-                }
-                //3달권
-                if(n+3 <= 13 && fee[2] + date[n] < date[n+3]) {
-                    date[n+3] = fee[2] + date[n];
-                }
-                //1년권
-                if(n+12 <= 13 && fee[3] + date[n] < date[n+12]) {
-                    date[n+12] = fee[3] + date[n];
-                }
-//               System.out.println(Arrays.toString(date));
+            // 각 달의 수영장 이용 계획 입력 받기
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= 12; i++) {
+                plan[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+            
+            for (int i = 1; i <= 12; i++) {
+            	dp[i] = Math.min(dp[i], dp[i-1] + plan[i] * fee[0]);
+            	
+            	dp[i] = Math.min(dp[i], dp[i-1] + fee[1]);
+            	
+            	if (i >= 3) {
+            		dp[i] = Math.min(dp[i], dp[i-3] + fee[2]);
+            	}else {
+            		dp[i] = Math.min(dp[i], fee[2]);
+            	}
+            	
             }
             
+           dp[12] = Math.min(dp[12], fee[3]);
             
-            System.out.printf("#%d %d\n", t, date[13]);
+            System.out.printf("#%d %d\n", t, dp[12]);
         }
         
 

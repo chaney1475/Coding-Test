b8c4db9628801bb6a3b8fcb4330846933bad649e
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-09-30 16:50:05 +0900

    [Gold III] Title: 앱, Time: 84 ms, Memory: 11928 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -4,20 +4,20 @@ import java.util.*;
 class Main {
 	// 앱
 
-	//실행중인 앱 활성화
-	
+	// 실행중인 앱 활성화
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		
+
 		int N = Integer.parseInt(st.nextToken()); // 갯수
 		int M = Integer.parseInt(st.nextToken()); // 바이트 수
-		
+
 		// M 바이트 확보하는데 가장 적게 드는 비용
-		// 
-		
+		//
+
 		int[] memory = new int[N]; // 바이트
 		int[] cost = new int[N]; // 끄는데 쓰이는 비용
 
@@ -26,37 +26,34 @@ class Main {
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			memory[i] = Integer.parseInt(st.nextToken());
-			total += memory[i];
+
 		}
-		
+
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			cost[i] = Integer.parseInt(st.nextToken());
+			total += cost[i];
 		}
-		
+
 //		System.out.println(Arrays.toString(memory));
 //		System.out.println(Arrays.toString(cost));
-		
+
 		int[] dp = new int[total + 1];
-		
-		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[0] = 0;
-		
+
 		for (int i = 0; i < N; i++) {
-			for (int j = total; j >= memory[i]; j--) {
-				if (dp[j - memory[i]] != Integer.MAX_VALUE) {
-				    dp[j] = Math.min(dp[j], dp[j - memory[i]] + cost[i]);
-				}
+			for (int j = total; j >= cost[i]; j--) {
+				dp[j] = Math.max(dp[j], dp[j - cost[i]] + memory[i]);
 			}
 		}
-		
-		 int ans = Integer.MAX_VALUE;
-	        for (int i = M; i <= total; i++) {
-	            if (dp[i] != Integer.MAX_VALUE) {
-	                ans = Math.min(ans, dp[i]);
-	            }
-	        }
-		
+
+		int ans = 0;
+		for (int i = 0; i <= total; i++) {
+			if (dp[i] >= M) {
+				ans = i;
+				break;
+			}
+		}
+
 		System.out.println(ans);
 	}
 }

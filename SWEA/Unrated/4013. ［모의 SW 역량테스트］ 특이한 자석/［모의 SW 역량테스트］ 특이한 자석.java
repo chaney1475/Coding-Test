4d7b3c1411dd187969e37861c4b4fa98e90ea225
서채연 <86292444+chaney1475@users.noreply.github.com>
2024-10-02 15:03:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 159 ms, Memory: 20,740 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -3,83 +3,106 @@ import java.util.*;
 
 public class Solution {
 
-	static int k;
-	static Deque<Integer>[] deq;
-	
+	static int[][] wheel;
+	static int[] start;
 	public static void main(String[] args) throws Exception {
+		// 최장 공통 부분 수열
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		int t = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc < t+1; tc++) {
-			k = Integer.parseInt(br.readLine());
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			int N = Integer.parseInt(br.readLine()); // 몇번 돌지
+			wheel = new int[4][8];
 			
-			deq = new ArrayDeque[4];
-			for (int i = 0; i< 4; i++) {
-				deq[i] = new ArrayDeque<>();
-			}
 			
+			int ans = 0;
+			
+			start = new int[4]; // 초기의 시작은 무조건 0
+			// 오른쪽은 2 왼쪽은 6
+			
+			//입력밪기
 			for (int i = 0; i < 4; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < 8; j++) {
-					deq[i].addLast(Integer.parseInt(st.nextToken()));
+					wheel[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			for (int i = 0; i < k; i++) {
+			//뭐할지 입력 받기
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				int idx = Integer.parseInt(st.nextToken())-1;
-				int dir = Integer.parseInt(st.nextToken());
 				
-				List<Integer> lst = new ArrayList<>(deq[idx]);
+				int a = Integer.parseInt(st.nextToken()) - 1; 
+				int b = Integer.parseInt(st.nextToken());
+				// a인덱스 바퀴를 b만큼 회전하자
 				
-				left(lst.get(6), idx-1, -dir);
-				right(lst.get(2), idx+1, -dir);
+				roll(a, -b);
 				
-				rotate(idx,dir);
 			}
 			
-			int result = 0;
+			int a = 0;
 			
-			if (deq[0].peekFirst() == 1) result += 1;
-            if (deq[1].peekFirst() == 1) result += 2;
-            if (deq[2].peekFirst() == 1) result += 4;
-            if (deq[3].peekFirst() == 1) result += 8;
+			int[] scores = {1, 2, 4, 8}; // 각 자석의 점수
+			for (int i = 0; i < 4; i++) {
+			    if (wheel[i][start[i]] == 1) { // S극인 경우
+			        ans += scores[i];
+			    }
+			}
 
-            System.out.println("#" + tc + " " + result);
+			
+			System.out.printf("#%d %d\n",t, ans);
+		}
+	}
+	static void roll(int i, int d) {
+		
+		// 오른쪽이 존재하는 경우
+		if( i + 1 < 4) {
+			// 오른쪽 휠의 왼쪽 확인
+			int rightLeft = wheel[i+1][(start[i+1] + 6) % 8];
+			if (rightLeft != wheel[i][(start[i] + 2) % 8]) {
+				right(i+1, d * (-1));
+			}
 			
 		}
 		
+		//왼쪽이 존재하는 경우
+		if (i - 1 >= 0) {
+			//왼쪽휠의 오른쪽 확인
+			int leftRight = wheel[i-1][(start[i-1] + 2) % 8];
+			if (leftRight != wheel[i][(start[i] + 6) % 8]) {
+				left(i-1, d * (-1));
+			}
+					
+		}
+		// 갱신은 무조건 끝까지 갓다가 하기
+		start[i] = (start[i] + d + 8) % 8;
 		
 	}
-	
-	private static void rotate(int idx, int dir) {
-		if (dir == 1) {
-			deq[idx].addFirst(deq[idx].pollLast());
-		} else {
-			deq[idx].addLast(deq[idx].pollFirst());
+	static void right(int i, int d) {
+		if(i + 1 < 4) {
+			// 오른쪽 휠의 왼쪽 확인
+			int rightLeft = wheel[i+1][(start[i+1] + 6) % 8];
+			if (rightLeft != wheel[i][(start[i] + 2) % 8]) {
+				right(i+1, d * (-1));
+			}
 		}
-	}
-	
-	private static void left(int num, int idx, int dir) {
-		if (idx < 0) return;
 		
-		List<Integer> lst = new ArrayList<>(deq[idx]);
-		if (lst.get(2) != num) {
-			left(lst.get(6), idx-1, -dir);
-			rotate(idx,dir);
+		start[i] = (start[i] + d + 8) % 8;
+	}
+	static void left(int i, int d) {
+		
+		if (i - 1 >= 0) {
+			//왼쪽휠의 오른쪽 확인
+			int leftRight = wheel[i-1][(start[i-1] + 2) % 8];
+			if (leftRight != wheel[i][(start[i] + 6) % 8]) {
+				left(i-1, d * (-1));
+			}
+					
 		}
+		
+		start[i] = (start[i] + d + 8) % 8;
 	}
 	
-	private static void right(int num, int idx, int dir) {
-        if (idx > 3) return;
-
-        List<Integer> lst = new ArrayList<>(deq[idx]);
-        if (lst.get(6) != num) {
-            right(lst.get(2), idx + 1, -dir);
-            rotate(idx, dir);
-        }
-    }
-
 }

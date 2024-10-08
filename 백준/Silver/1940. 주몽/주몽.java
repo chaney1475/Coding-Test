4b7206a558c3d1e675143aa859be362427bcaf40
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-08 13:12:24 +0900

    [Silver IV] Title: 주몽, Time: 128 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,29 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int M = Integer.parseInt(bf.readLine());
-        int[] A = new int[N];
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        for(int i = 0; i < N;i ++){
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(A);
-        int cnt = 0;
-        int i = 0;
-        int j = N-1;
+	
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (i < j){
-            if (A[i] + A[j] < M) i++;
-            else if (A[i] + A[j] > M) j --;
-            else {cnt++; i++; j--;}
-        }
-        System.out.println(cnt);
-    }
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] A = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		
+		int s = 0;
+		int e = N-1;
+		
+		int cnt = 0;
+		
+		while(s < e) {
+			int num = A[s] + A[e];
+			if (num == M) {
+				cnt++;
+				s++;
+				e--;
+			}
+			else if (num < M) { // 현재 숫자보다 만들어진 수가 작을 때
+				s++;
+				
+			}else {// 현재 숫자가 더 큰경우
+				e--;
+			}
+			
+		}
+		System.out.println(cnt);
+		
+	}
+	
 }

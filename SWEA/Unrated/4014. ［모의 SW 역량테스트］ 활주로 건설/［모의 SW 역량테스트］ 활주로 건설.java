675487f0bfb5ccf1461699bc8204a07d8f551303
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-10 14:27:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 158 ms, Memory: 24,080 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -3,85 +3,94 @@ import java.util.*;
 
 public class Solution {
 
-	static int ans;
-	static int[][] map;
-	static int N;
-	static int X;
+    static int ans;
+    static int[][] map;
+    static int N;
+    static int X;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-		for (int t = 1; t <= T; t++) {
+        for (int t = 1; t <= T; t++) {
 
-			ans = 0;
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			X = Integer.parseInt(st.nextToken()); // 경사로의 길이
+            ans = 0;
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            X = Integer.parseInt(st.nextToken()); // 경사로의 길이
 
-			map = new int[N][N];
+            map = new int[N][N];
 
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			ans = 0;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            ans = 0;
 
-			for (int i = 0; i < N; i++) {
-				if ((check(map[i]))) {
-					ans++;
-				}
-			}
+            for (int i = 0; i < N; i++) {
+                if ((check(map[i]))) {
+                    ans++;
+                }
+            }
 
-			for (int j = 0; j < N; j++) {
-				int[] a = new int[N];
+            for (int j = 0; j < N; j++) {
+                int[] a = new int[N];
+                for (int i = 0; i < N; i++) {
+                    a[i] = map[i][j];
+                }
 
-				for (int i = 0; i < N; i++) {
-					a[i] = map[i][j];
-				}
+                if (check(a)) {
+                    ans++;
+                }
+            }
 
-				if (check(a)) {
-					ans++;
-				}
-			}
+            System.out.printf("#%d %d\n", t, ans);
+        }
+    }
 
-			System.out.printf("#%d %d\n", t, ans);
-		}
-	}
+    static boolean check(int[] arr) {
 
-	static boolean check(int[] arr) {
+        int before = arr[0];
+        int cnt = 1;
 
-		boolean[] used = new boolean[N];
-		for (int i = 0; i < N - 1; i++) {
-			if (arr[i] == arr[i + 1])
-				continue;
-			if (Math.abs(arr[i] - arr[i + 1]) > 1)
-				return false;
+        for (int i = 1; i < N; i++) {
 
-			if (arr[i] - arr[i + 1] == 1) {
-				// 내려감
-				for (int j = 1; j <= X; j++) {
-					// 범위 내에 잇어야하고, 그 다음 수와 일치해야하고, 사용하지 않았어야함
-					if (i + j >= N || arr[i + j] != arr[i + 1] || used[i + j])
-						return false;
-					used[i + j] = true;
-				}
-			} else if (arr[i] - arr[i + 1] == -1) {
-				// 올라감
-				for (int j = 0; j < X; j++) {
-					if (i - j < 0 || arr[i - j] != arr[i] || used[i - j])
-						return false;
-					used[i - j] = true;
-				}
-			}
+            int diff = arr[i] - before;
 
-		}
-		return true;
+            if (Math.abs(diff) > 1) { // 높이 차가 1보다 크면 경사로를 설치할 수 없음
+                return false;
+            }
 
-	}
+            if (before == arr[i]) {
+                cnt++; // 같은 높이의 길이를 셈
+            } else if (before < arr[i]) { // 오르막일 경우
+                if (cnt < X) return false; 
 
+                cnt = 1; 
+                before = arr[i];
+            } else { // 내리막일 경우
+                int now = arr[i];
+                int tmpCnt = 1; // 경사로를 놓을 길이 체크
+                i++;
+
+                while (i < N && tmpCnt < X) { // 경사로를 놓기 위해 다음 X칸 확인
+                    if (arr[i] != now) { // 경사로를 놓을 칸이 모두 같은 높이가 아니면 실패
+                        return false;
+                    }
+                    i++;
+                    tmpCnt++;
+                }
+
+                if (tmpCnt < X) return false; // 경사로의 길이가 부족하면 불가능
+                i--; // 다음 반복에서 i가 정상적으로 진행되도록 복구
+                cnt = 0; // 경사로를 놓았으니 초기화
+                before = arr[i]; // 현재 높이로 업데이트
+            }
+        }
+        return true;
+    }
 }

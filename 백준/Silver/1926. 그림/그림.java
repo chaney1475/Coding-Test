ac930cc951bb129658f7601e98b92d2ab40a64d2
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-10 20:26:37 +0900

    [Silver I] Title: 그림, Time: 316 ms, Memory: 42456 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,68 +1,89 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n,m;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int count = 0;
-    static int[][] pos = {{0,-1},{-1,0},{0,1},{1,0}};
+    static int N;
+    static int M;
+    static int[][] map;
 
-    static void dfs(int x, int y){
-        visited[x][y] = true;
-        count++;
+    static int size;
 
-        for (int i = 0; i < 4; i++){
-            int nx = x + pos[i][0];
-            int ny = y + pos[i][1];
+    static class Point{
+        int x, y;
 
-            if (nx < 0 || ny < 0 || nx >= n || ny >= m || graph[nx][ny] == 0 || visited[nx][ny]){
-                continue;
-            }
-
-            dfs(nx, ny);
-
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int[] dx = new int[] {0,0,-1,1};
+    static int[] dy = new int[] {1,-1,0,0};
 
-        n = sc.nextInt();
-        m = sc.nextInt();
-        boolean zero = true;
-        List<Integer> answer = new ArrayList<>();
-        graph = new int[n][m];
-        visited = new boolean[n][m];
+    static void bfs(int a, int b){
 
-        for(int i = 0; i< n; i++){
-            for (int j = 0; j < m; j++){
-                graph[i][j] = sc.nextInt();
-                if(graph[i][j] == 1){
-                    zero = false;
+        int tmp = 1;
+
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(a, b));
+
+        map[a][b] = 2;
+
+        while( !q.isEmpty()){
+            Point now  = q.poll();
+            int x = now.x;
+            int y = now.y;
+
+            for (int d = 0; d < 4; d++){
+                int nx = dx[d] + x;
+                int ny = dy[d] + y;
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M){
+                    continue;
+                }
+                if (map[nx][ny] == 1){
+                    map[nx][ny] = 2;
+                    tmp ++;
+                    q.add(new Point(nx, ny));
+                }
+
+            }
+        }
+        size = Math.max(size, tmp);
+
+    }
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        int cnt = 0;
+        size = 0;
+
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++){
+                if (map[i][j] == 1){
+                    cnt++;
+                    bfs(i, j);
                 }
             }
         }
 
-        if(zero){
-            System.out.println("0");
-            System.out.println("0");
-            return;
-        }
+        System.out.println(cnt);
+        System.out.println(size);
 
-        for (int i = 0; i< n ; i ++){
-            for (int j = 0; j < m; j++){
-                count = 0;
-                if (!visited[i][j] && graph[i][j] == 1 ){
-                    dfs(i,j);
-                    answer.add(count);
-                }
-            }
-        }
-        Collections.sort(answer);
-        System.out.println(answer.size());
-        System.out.println(answer.get(answer.size() - 1));
     }
 }

e00d4db8c394524f24f83759a2b84f6f1f2ee7cc
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-15 14:06:08 +0900

    [Silver II] Title: 쇠막대기, Time: 204 ms, Memory: 17996 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
         int totalPieces = 0;
 
         for (int i = 0; i < input.length(); i++) {
@@ -14,6 +15,7 @@ public class Main {
                 stack.push('(');
             } else {
                 stack.pop();
+                
                 if (input.charAt(i - 1) == '(') {
                     totalPieces += stack.size();
                 } else {

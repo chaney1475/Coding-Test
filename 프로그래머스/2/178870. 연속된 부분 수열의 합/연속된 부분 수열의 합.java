d40383fe0fd183987f7521c1be6458c9ad66903b
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-16 17:01:06 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 30.50 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -12,31 +12,23 @@ class Solution {
         long K = (long) k;
         int N = a.length;
         
-        long tmp = a[s];
+        long tmp = 0;
         
         long len = N + 1;
         
         //작으면 e를 늘림 크면 크면 왼쪽을 오른쪽으로 함
         while(e < N) {
-            //갱신
-            if(tmp == K && (e - s + 1) < len){
-                answer[0] = s;
-                answer[1] = e;
-                len = e - s + 1;
+            tmp += a[e];
+            while(tmp >= K){
+                if(tmp == K && (e - s + 1) < len){
+                    answer[0] = s;
+                    answer[1] = e;
+                    len = e - s + 1;
+                }
+                tmp -= a[s];
+                s++;
             }
-            
-            if (e == N - 1 && s == N - 1) break;
-            
-            //조정
-            if (tmp <= K){
-                if (e == N - 1) break;
-                e++;
-                tmp += a[e];
-            }else {
-                if (s == N - 1) break;
-                tmp -= a[s++];
-            }
-            
+            e++;
         }
         return answer;
         

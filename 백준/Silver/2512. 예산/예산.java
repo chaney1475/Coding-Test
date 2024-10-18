98114df3a4f42606f8c693ac622fddb8d60b8474
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-18 18:19:28 +0900

    [Silver II] Title: 예산, Time: 100 ms, Memory: 13124 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,51 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
-
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-
-
-	public static void main(String[] args) throws IOException {
-		
-		int N = Integer.parseInt(br.readLine());
-		long[] A = new long[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int limit = Integer.parseInt(br.readLine());
-		
-		Arrays.sort(A);
-		long s = 1;
-		long e = A[N-1];
-		long m;
-		long ans = 0;
-		
-		while (s <= e) {
-			m = (s + e) / 2;
-			
-			long sum = 0;
-			for (int i = 0; i < N; i++) {
-				if (m > A[i]) {
-					sum += A[i];
-				}else {
-					sum += m;
-				}
-			}
-			
-			if (sum <= limit) {
-				ans = m;
-				s = m + 1;
-			}else {
-				e = m - 1;
-			}
-		}
-		System.out.println(ans);
-		
-	}
-
+    static int N;
+    
+  public static void main(String args[]) throws Exception{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st;
+      
+      N = Integer.parseInt(br.readLine());
+      int[] a = new int[N];
+      
+      st = new StringTokenizer(br.readLine());
+      long e = 0;
+      
+      for (int i = 0; i < N; i++){
+          a[i] = Integer.parseInt(st.nextToken());
+          e = Math.max(a[i], e);
+      }
+      long limit = Long.parseLong(br.readLine());
+      
+      long s = 1;
+      long answer = -1;
+      
+      while(s <= e){
+          long m = (s + e) / 2;
+          
+          long sum = 0;
+          for (int i = 0; i < N; i++){
+              sum += (long) Math.min(m, (long) a[i]);
+          }
+          
+          if (sum <= limit){
+              answer = m;
+              s = m + 1;
+          }else{
+              e = m - 1;
+          }
+          
+      }
+      
+      System.out.println(answer);
+  }
 }

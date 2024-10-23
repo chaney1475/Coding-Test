9c645d2d8831332ce5ebed61205dd43af71d4ba6
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-23 19:49:13 +0900

    [level 2] Title: 디펜스 게임, Time: 96.26 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,45 +1,25 @@
 import java.util.*;
 
 class Solution {
-    static int[] last;
-    public int solution(int n, int K, int[] enemy) {
-        int ans = 0;
-        int N = enemy.length;
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>((a,b) -> b - a);
         
-        PriorityQueue<Integer> q = new PriorityQueue<>((a,b) -> b-a);
-        int left = n; // 나의 병사수
-        int last = -1; // 마지막 위치
-        
-        //내가 사용한 애들을 큐에 넣어서 K에 사용할 걸 찾기
-        
-        for (int i = 0; i < N; i++){
-            int now = enemy[i]; //현재 빼야하는 수
+        for (int i = 0; i < enemy.length; i++){
+            n -= enemy[i];
+            q.add(enemy[i]); 
             
-            if (left >= now){
-                left -= now;
-                last = i;
-                q.add(now);
-            }else{
-                if (K == 0) {
+            if (n < 0){
+                if (!q.isEmpty() && k > 0){
+                    n += q.poll();
+                    k--;
+                }else{
                     break;
                 }
-                if (!q.isEmpty() && q.peek() > now && q.peek() + left >= now){
-                    left += q.poll();
-                    left -= now;
-                    last = i;
-                    K--;
-                    q.add(now);
-                }else{
-                     last = i;
-                     K--;
-                        
-                }
             }
-            
+            answer = i;
         }
         
-        return last + 1;
+        return answer + 1;
     }
-    
-    
 }

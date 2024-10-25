77b594a6d127318e82eb195b96c647faf421ea15
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-10-25 10:13:49 +0900

    [level 3] Title: 기지국 설치, Time: 0.55 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -3,29 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int ans = 0;
+        //비어있는 곳을 범위로 나누기
+        // 범위는 w * 2 + 1
         
-        int S = stations.length;
-        int sta = 1;
+        int end = 1; // 처음으로 범위가 아닌곳을 매번 저장한다
+        // 범위인 곳이 나오면 그것의 처음으로 범위가 아닌 값을 넣어준다
         
-        int W = w * 2 + 1;
-        
-        for (int now : stations){
-            int end = now - w;
-            
-            if(sta < end) {
-                int len = end - sta;
-                int tmp = (len / W);
-                if (len % W != 0) tmp++;
-                ans += tmp;
+        for (int i = 0; i < stations.length; i++){
+            int now = stations[i];
+            if (now - w > end){
+                ans += (now - end + w) / (w * 2 + 1);
             }
-            sta = now + w + 1;
+            end = now + w + 1;
         }
         
-        if (sta <= n){
-            int len = n - sta + 1;
-            int tmp = (len / W);
-            if (len % W != 0) tmp++;
-            ans += tmp;
+        if (end <= n){
+            ans += (n - end + 1 + w * 2) / (w * 2 + 1);
         }
         
         return ans;

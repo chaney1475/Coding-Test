a2bcaae3b86e4ecbf3becd9b2372f25e3413dafe
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2024-11-04 15:18:37 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 64 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -1,37 +1,47 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        
-        for (int t = 0; t < T; t++) {
-            String[] input = br.readLine().trim().split(" ");
-            int x = Integer.parseInt(input[0]);
-            int y = Integer.parseInt(input[1]);
-            
-            long d = y - x;
-
-            int k = 0;  
-            long max = 0;
-            int move = 1;
-
-            while (max < d) {
-                k++;
-                
-                if (k % 2 == 1) {
-                    max += move;
-                } else {
-                    max += move;
-                    move++;
-                }
-            }
-
-            sb.append(k).append("\n");
-        }
-
-        System.out.print(sb);
-    }
+  public static void main(String args[]) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st;
+      
+      int T = Integer.parseInt(br.readLine());
+      
+      StringBuilder sb = new StringBuilder();
+      
+      for (int t = 0; t < T; t++){
+          st = new StringTokenizer(br.readLine());
+          int a = Integer.parseInt(st.nextToken());
+          int b = Integer.parseInt(st.nextToken());
+          int d = b - a;
+          
+          int s = 0;
+          int e = 100000;
+          
+          int ans = 0;
+          
+          while(s <= e){
+              int m = (s + e) / 2;
+              if (getDist(m) >= d){
+                  ans = m; 
+                  e = m - 1;
+              }else{
+                  s = m + 1;
+              }
+          }
+          sb.append(ans).append("\n");
+      }
+      System.out.print(sb);
+      
+  }
+  
+  static long getDist(int K){
+      long k = K / 2;
+      if (K % 2 == 0){
+          return k * (k + 1) ;
+      }
+      return k * (k + 1) + k + 1;
+      
+  }
 }

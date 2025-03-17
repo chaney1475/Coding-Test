2335a55b8f96905499e0b46dde44cad840e12689
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2025-03-17 21:40:34 +0900

    [level 3] Title: 가장 먼 노드, Time: 120.36 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,64 +1,63 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
-    static int N;
+    ArrayList<Integer>[] adj;
+    int n;
+    int len;
     
-    static ArrayList<Integer>[] adj;
-    static int maxD;
-    static int count;
-    
+    int ans;
+    int[] dist;
     
     public int solution(int n, int[][] edge) {
-        N  = n;
+        this.n = n;
         
-        visited = new boolean[n + 1];
         adj = new ArrayList[n+1];
-        count = 0;
         
-        for (int i = 1; i <= N; i++){
+        for (int i = 0; i < n+ 1; i++){
             adj[i] = new ArrayList<>();
         }
         
-        for (int i = 0; i < edge.length; i++){
-            int[] now = edge[i];
-            adj[now[1]].add(now[0]);
-            adj[now[0]].add(now[1]);
+        len = edge.length;
+        
+        dist = new int[n+1];
+        Arrays.fill(dist, -1);
+        
+        for (int i = 0; i < len; i++){
+            int a = edge[i][0];
+            int b = edge[i][1];
+            adj[a].add(b);
+            adj[b].add(a);
         }
         
-        bfs();
+        // 1에서 가장 멀리 떨어져있는 노드
         
-        return count;
+        dist[1] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(1);
+        while(!q.isEmpty()){
+            int now = q.poll();
+            for (int next : adj[now]){
+                if (dist[next] != -1) continue;
+                dist[next] = dist[now] + 1;
+                q.add(next);
+            }
+        }
+        int max = 0;
+        int ans = 0;
+        
+        for (int i = 1; i < n + 1 ; i++){
+            int now = dist[i];
+            if (max < now){
+                max = now;
+                ans = 1;
+            } else if (max == now){
+                ans++;
+            }
+        }
+        
+        return ans;
         
     }
     
-    static void bfs(){
-        
-        visited[1] = true;
-        Queue<int[]> q = new LinkedList<>();
-        for (Integer next : adj[1]){
-            visited[next] = true;
-            q.add(new int[] {next,1});
-        }
-        
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            int node = now[0];
-            int d = now[1];
-            
-            if (maxD == d) count++;
-            else if (d > maxD){
-                maxD = d;
-                count = 1;
-            }
-            
-            for (Integer next : adj[node]){
-                if (visited[next]) continue;
-                visited[next] = true;
-                q.add(new int[] {next, d + 1});
-            }
-        }
-        
-        
-    }
 }

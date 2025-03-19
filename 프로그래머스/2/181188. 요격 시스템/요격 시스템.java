21150d111f0f50290879aa7084c1cccba49fa8fb
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2025-03-19 10:43:22 +0900

    [level 2] Title: 요격 시스템, Time: 537.46 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,36 +1,37 @@
-/*
-먼저 시작하는 순서 대로, 늦게끝나는 순서대로 넣어야하기 때문에
-*/
 import java.util.*;
+
 class Solution {
     public int solution(int[][] targets) {
-        int ans = 0;
-        
-        int N = targets.length;
         
         Arrays.sort(targets, (a,b) -> {
             if (a[0] != b[0])
-               return Integer.compare(a[0], b[0]);
+                return a[0] - b[0];
             else
-                return Integer.compare(b[1], a[1]);
+                return b[1] - a[1];
         });
         
-        int nS = -1;
-        int nE = -1;
+        int N = targets.length;
+        
+        int start = -1;
+        int end = -1;
+        int ans = 0;
         
         for (int i = 0; i < N; i++){
-            int[] now = targets[i];
-            int start = now[0];
-            int end = now[1];
+            int nowS = targets[i][0];
+            int nowE = targets[i][1];
             
-            if (start >= nE){
+            //end 보다 먼저 시작하는 경우
+            
+            if (end > nowS){
+                start = Math.max(start, nowS);
+                end = Math.min(end, nowE);
+            }else{
+                
                 ans++;
-                nS = start;
-                nE = end;
-            }else {
-                nS = Math.max(start, nS);
-                nE = Math.min(end, nE);
+                start = nowS;
+                end = nowE;
             }
+            
         }
         
         return ans;

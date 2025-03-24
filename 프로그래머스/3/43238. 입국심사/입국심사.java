7fcffe98df7084ed6a35d3885f5cd2b2e4bd795b
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2025-03-24 20:12:10 +0900

    [level 3] Title: 입국심사, Time: 75.52 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,49 +2,36 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
         
-        long s = 1L;
-        long e = 1L;
+        int M = times.length;
         
-        long maxTime = 0L;
-        
-        int len = times.length;
-        
-        for (int i = 0; i < len; i++){
-            maxTime = Math.max(maxTime, (long) times[i]);
+        long maxN = -1;
+        for (int i = 0; i < M; i++){
+            maxN = Math.max(maxN, times[i]);
         }
         
-        e = n * maxTime;
+        long s = 1;
+        long e =  maxN * n;
         
-        long ans = 0L;
+        long ans = -1;
         
-        while( s <= e ){
-            long m = (s + e) / 2;
+        while (s <= e){
+            long mid = (s + e) / 2;
+            long tmp = 0;
             
-            //나와야하는 수보다 크거나 같은 경우 답임
-            if (getCnt(m, times) >= n){
-                //가능한 경우 시간을 줄이자
-                ans = m;
-                e = m - 1;
-                
+            // 몇명이나 들어올 수 있는지
+            for (int i = 0; i < M; i++){
+                tmp += mid / times[i];
+            }
+            
+            if (tmp >= n){
+                ans = mid;
+                e = mid - 1;
             }else{
-                s = m + 1;
+                s = mid + 1;
             }
         }
         
         return ans;
-    }
-    
-    long getCnt(long m, int[] times){
-        // 각 times에서 해당 초수로 가능한 사람수 구하기!
-        
-        long sum = 0L;
-        for (int i = 0; i < times.length; i++){
-            sum += m / times[i];
-        }
-        
-        return sum;
-        
     }
 }

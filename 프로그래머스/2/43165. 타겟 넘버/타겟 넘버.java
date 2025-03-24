3fb7fda948181464f5867537401479cf41061f3d
chaney1475 <86292444+chaney1475@users.noreply.github.com>
2025-03-24 21:10:59 +0900

    [level 2] Title: 타겟 넘버, Time: 10.76 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,47 +1,49 @@
 import java.util.*;
 
 class Solution {
-    static int N;
-    static int tar;
-    
-    static int[] nums;
-    static int ans;
-    
+    int[] nums;
+    int N;
+    int target;
+    int ans;
     public int solution(int[] numbers, int target) {
-        N = numbers.length;
-        tar = target;
-        
         nums = numbers;
+        this.target = target;
+        N = numbers.length;
         
         ans = 0;
-        
-        dfs(0,0);
+        dfs(0,0); // 0번의 숫자를 더할건가 뺄건가 
         
         return ans;
     }
     
-    static void dfs(int cnt, int tmp){
+    void dfs(int now, int tmp){
+        
+        // 두개의 가지 치기
+        
+        // 모두를 더해도 여전히 작다 타겟보다        
+        // 모두를 빼도 여전이 크다 타켓보다 
         
         int sum = 0;
-        for (int i = cnt; i < N; i++){
+        for (int i = now; i < N; i++){
             sum += nums[i];
         }
         
-        if(tmp + sum < tar) return;
-        if(tmp - sum > tar) return;
+        if (target > sum + tmp){
+            return;
+        }
+        if (target < tmp - sum){
+            return;
+        }
         
-        if(cnt == N){
-            if (tmp == tar){
+        if (now == N ){
+            if (target == tmp){
                 ans++;
             }
             return;
         }
         
-        dfs(cnt + 1, tmp + nums[cnt]);
-        dfs(cnt + 1, tmp - nums[cnt]);
+        dfs(now + 1, tmp - nums[now]);
+        dfs(now + 1, nums[now] + tmp);
         
     }
-    
-    
-    
 }

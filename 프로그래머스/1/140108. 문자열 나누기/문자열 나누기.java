25ef0e10c52606d096310a5a942bb6284b85aca3
chaney1475 <chaney2225@gmail.com>
2025-12-23 17:46:05 +0900

    [level 1] Title: 문자열 나누기, Time: 0.74 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,36 +1,32 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        int n = s.length();
+        int xCnt = 0;
+        int yCnt = 0;
         
-        //같아지는 경우 cnt++;
-        char x = 'A';
+        char first = s.charAt(0);
         
-        int xCnt = 0; //맨 앞에 있는 문자의 갯수
-        int yCnt = 0; // 다르게 되는 문자의 갯수
-        
-        for (int i = 0; i < s.length(); i++){
-            
-            if(xCnt == 0 && 0 == yCnt){
-                x = s.charAt(i);
+        for (int i = 0; i < n; i++){
+            char cur = s.charAt(i);
+            if (
+                xCnt == 0 &&
+                yCnt == 0){
+                first = cur;
             }
-            
-            if(s.charAt(i) == x){
+            if (cur == first){
                 xCnt++;
             }else{
                 yCnt++;
             }
-            
-            //갯수를 세고나서 같으면 또 뒤에보기
-            if(xCnt == yCnt && xCnt != 0){
+            if(xCnt == yCnt){
+                answer++;
                 xCnt = 0;
                 yCnt = 0;
-                answer++;
             }
         }
         
-        if(xCnt != yCnt){
+        if (xCnt > 0 || yCnt > 0){
             answer++;
         }
         

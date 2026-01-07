69fb0052bca7c4725a1cf80a1b9fa6de42108057
chaney1475 <chaney2225@gmail.com>
2026-01-07 20:01:51 +0900

    [level 1] Title: 햄버거 만들기, Time: 56.81 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 157 MB, 시간: 4414.24 ms
+메모리: 142 MB, 시간: 56.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 10일 22:30:57
+2026년 01월 07일 20:01:48
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,37 +1,46 @@
 import java.util.*;
+// 1,  2,   3
+// 빵, 야채, 고기
+// 야채, 고기, 빵
 class Solution {
     public int solution(int[] ingre) {
         
-        ArrayList<Integer> list = new ArrayList<>();
-        int N = ingre.length;
+        int ans = 0;
         
-        for (int i = 0; i < N; i++){
-            list.add(ingre[i]);
-        }
+        int[] order = new int[]{1,3,2,1}; //빵 야채 고기 빵 
         
-        int cnt = 0;
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        ArrayDeque<Integer> tmp = new ArrayDeque<>();
         
-        int i = 0; 
-        int k = ingre.length - 3;
+        int n = ingre.length;
         
-        while( i < k){
-            if (list.get(i) == 1 && list.get(i+1) == 2 &&
-               list.get(i+2) == 3 && list.get(i+3) == 1)
-            {
-                list.remove(i+3);
-                list.remove(i+2);
-                list.remove(i+1);
-                list.remove(i);
-                k -= 4;
-                if(i-4 >= -1) i -= 4;
-                else i = -1;
-                cnt++;
+        for (int i = 0; i < n; i++) {
+            int now = ingre[i]; // 현재 재료
+            dq.add(now);
+            
+            if( dq.peekLast() == 1 && dq.size() >= 4){
+                
+                for (int j = 0; j < 4; j++) {
+                    if (order[j] == dq.peekLast()) {
+                        tmp.add(dq.pollLast());
+                    }else{
+                        break;
+                    }
+                }
+                
+                if (tmp.size() == 4){
+                    ans += 1;
+                    tmp.clear();
+                } else {
+                    while (tmp.size() > 0){
+                        dq.add(tmp.pollLast());
+                    }
+                }
             }
-            i++;
+            
+            
         }
         
-        return cnt;
-        
+        return ans;
     }
-    
 }

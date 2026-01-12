70ccc887faee02297fb4497c72062f53b6149b5c
chaney1475 <chaney2225@gmail.com>
2026-01-12 13:06:02 +0900

    [level 1] Title: 숫자 짝꿍, Time: 63.63 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -2,31 +2,44 @@ import java.util.*;
 
 class Solution {
     public String solution(String X, String Y) {
+        String answer = "";
+        int[] numsX = new int[10];
+        int[] numsY = new int[10];
+        // 0부터 9까지 저장
         
-        int[] x = new int[10];
-        int[] y = new int[10];
-        
-        int nx = X.length();
-        int ny = Y.length();
-        
-        for (int i = 0; i < nx; i++){
-            x[X.charAt(i) - '0']++;
+        for (int i = 0; i < X.length(); i++) {
+            numsX[X.charAt(i) - '0']++;
         }
-        for (int i = 0; i < ny; i++){
-            y[Y.charAt(i) - '0']++;
+        
+        for (int i = 0; i < Y.length(); i++) {
+            numsY[Y.charAt(i) - '0']++;
         }
         
         StringBuilder sb = new StringBuilder();
         
-        for (int i = 9; i >= 0; i--){
-            int cnt = Math.min(x[i], y[i]);
-            for (int c = 0; c < cnt; c++){
-                sb.append(i);
+        int sum = 0;
+        for (int i = 9; i >= 1; i--) {
+            int min = Math.min(numsX[i], numsY[i]);
+            sum += min;
+            if (min > 0) {
+                for (int j = 0; j < min; j++) {
+                    sb.append(i);
+                }
             }
         }
-        if (sb.length() == 0) return "-1";
-        else if (sb.charAt(0) =='0') return "0"; 
         
+        int zeroCnt = Math.min(numsX[0], numsY[0]);
+        if (sb.length() > 0) {
+            for (int i = 0; i < zeroCnt; i++) {
+                sb.append(0);
+            }
+        } else if ( zeroCnt > 0){
+            sb.append(0);
+        }
+            
+        if (sb.length() == 0) {
+            sb.append(-1);
+        }
         return sb.toString();
     }
 }

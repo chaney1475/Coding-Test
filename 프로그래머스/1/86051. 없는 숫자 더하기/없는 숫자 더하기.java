f3c3a9435252d3806aeea4c35066aa8053412b6e
chaney1475 <chaney2225@gmail.com>
2026-03-03 12:37:47 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        int[] nums = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            nums[numbers[i]] = 1;
+        }
+        for (int i = 0;i < 10; i++) {
+            if (nums[i] == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}

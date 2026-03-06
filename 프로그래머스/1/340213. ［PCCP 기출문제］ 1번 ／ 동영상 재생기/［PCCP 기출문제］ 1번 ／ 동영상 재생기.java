8008a6e8b7658b604bf0f280340461eaf31b792e
chaney1475 <chaney2225@gmail.com>
2026-03-06 12:51:37 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 30.61 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,57 +1,73 @@
 import java.util.*;
-import java.io.*;
-
+// 10초전 10초 후 오프닝 뛰
+// prev -10 마이너스면 0으로
+// next +10 끝이면 시간 끝
+// 오프닝 건너뛰기 <= 현재 <= -> op_end
 class Solution {
+    static int videoLen;
+    static int opStart;
+    static int opEnd;
+    
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-
-        int len = getTime(video_len); // 비디오 길이
-        int time = getTime(pos); // 현재 위치
-        int start = getTime(op_start); //  오프닝 시작
-        int end = getTime(op_end); //  오프닝 끝
+        String answer = "";
         
-        for (String command : commands) {
+        int cur = timeToNum(pos); // 현재 위치
+        videoLen = timeToNum(video_len); // 비디오 길이
+        opStart = timeToNum(op_start); // 오프닝 시작
+        opEnd = timeToNum(op_end); // 오프닝 끝
+        
+        
+        int c_len = commands.length;
+        for (int i = 0; i < c_len; i++) {
             
-            if (time >= start && time <= end) {
-                time = end; // 오프닝 건너뛰기
-            }
-
-            if (command.equals("next")) {
-                time += 10;
-            } else if (command.equals("prev")) {
-                time -= 10;
-            }
-
-            // 범위 조정
-            if (time < 0) {
-                time = 0; // 시작 위치로 조정
-            } else if (time >= len) {
-                time = len; // 마지막 위치로 조정
-            }
-        }
-
-        // 마지막으로 오프닝 구간에 있는지 체크
-        if (time >= start && time <= end) {
-            time = end; // 오프닝 끝으로 이동
+            cur = goOpening(cur);
+            String command = commands[i];
+            if (command.equals("prev")) {
+                cur = getFixed(cur - 10);
+            } else {
+                cur = getFixed(cur + 10);
+            } 
+            cur = goOpening(cur);
         }
         
-        String t1 = String.valueOf(time / 60);
-        String t2 = String.valueOf(time % 60);
+        int min = cur / 60;
+        int sec = cur % 60;
         
-        while(t1.length() < 2){
-           t1 = "0" + t1; 
+        String minStr = "";
+        String secStr = "";
+        if (min / 10 < 1) {
+            minStr += "0";
         }
-        while(t2.length() < 2){
-             t2 = "0" + t2; 
+        if (sec / 10 < 1) {
+            secStr += "0";
         }
+        minStr += min;
+        secStr += sec;
         
-        return t1 + ":" + t2;
-
+        answer = minStr + ":" + secStr;
+        
+        return answer;
     }
     
-    static int getTime(String time){
-        String[] s = time.split(":");
-        int ans = Integer.parseInt(s[0]);
-        ans = ans * 60 + Integer.parseInt(s[1]);
-        return ans;
+    public int timeToNum(String str){
+        String[] st = str.split(":");
+        int min = Integer.valueOf(st[0]);
+        int sec = Integer.valueOf(st[1]);
+        return min * 60 + sec;
+    }
+    
+    public int getFixed(int cur) {
+        if (cur < 0) {
+            return 0;
+        } else if (cur > videoLen){
+            return videoLen;
+        } 
+        return cur;
+    }
+    public int goOpening(int cur) {
+        if (opStart <= cur && cur <= opEnd) {
+            return opEnd;
+        }
+        return cur;
     }
 }

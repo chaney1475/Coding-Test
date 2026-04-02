f0485e6d911e101497d13558a903d72cfef5b54b
chaney1475 <chaney2225@gmail.com>
2026-04-03 00:22:29 +0900

    [level 2] Title: 시소 짝꿍, Time: 28.08 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,61 +1,36 @@
 import java.util.*;
-/*
-100 2명
-210 1명
-* 2 랑 같기
-*/
-import java.util.*;
 
 class Solution {
-    int[][] c = new int[3][2];
     public long solution(int[] weights) {
-        
-        long ans = 0;
-        
-        
+        long answer = 0;
+        int n = weights.length;
+
         Map<Integer, Integer> map = new HashMap<>();
-        
-        int N = weights.length;
-        
-        for (int i = 0; i < N; i++){
-            map.put(weights[i], map.getOrDefault(weights[i], 0) + 1);
+
+        for (int i = 0; i < n; i++) {
+            map.put(weights[i], map.getOrDefault(weights[i],0) + 1);
         }
-        int n = map.size();
         
-        int[] list = new int[n];
-        int[] cnt = new int[n];
-        int idx = 0;
-        
-        for (Map.Entry<Integer, Integer> en: map.entrySet()){
-            list[idx] = en.getKey();
-            cnt[idx++] = en.getValue();
-        }
-        c[0] = new int[]{1,2};
-        c[1] = new int[]{2,3};
-        c[2] = new int[]{3,4};
-        
-        for (int i = 0; i < n; i++){
-            // 나 자신인 경우
-            ans += ((long)(cnt[i] - 1) * (long)(cnt[i])) / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             
-            for (int j = i + 1; j < n; j++){
-                if (check(list[i], list[j])){
-                    ans += (long) cnt[i] * (long) cnt[j];
-                }
+            int num = entry.getKey();
+            int a = entry.getValue();
+            
+            answer += (long)a * (long)(a - 1) / 2;
+            
+            if (num % 2 == 0) {
+                int pair = num / 2 * 3;
+                int b = map.getOrDefault(pair, 0);
+                answer += (long)a * (long)b;
             }
+            if (num % 3 == 0) {
+                int pair = num / 3 * 4;
+                int b = map.getOrDefault(pair, 0);
+                answer += (long)a * (long)b;
+            }
+            answer += (long) map.getOrDefault(num * 2, 0) * (long) a;
         }
         
-        return ans;
-    }
-    boolean check(long x, long y){    
-        for (int i = 0; i < 3; i++){
-            int a = c[i][0];
-            int b = c[i][1];
-            
-            if (a * x == y * b || a * y == b * x){
-                return true;
-            }
-        }
-        return false;
+        return answer;
     }
 }

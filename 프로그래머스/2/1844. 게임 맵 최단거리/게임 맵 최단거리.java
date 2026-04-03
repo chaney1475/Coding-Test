3d5a0a8d32a26a48100df0c30e81bfb56a6884e6
chaney1475 <chaney2225@gmail.com>
2026-04-03 19:06:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.48 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,50 +1,35 @@
 import java.util.*;
 
 class Solution {
+
+    int[] dx = {0,0,-1,1};
+    int[] dy = {-1,1,0,0};
+
     public int solution(int[][] maps) {
-        
-        int N = maps.length;
-        int M = maps[0].length;
-        
-        int ans = -1;
-        
-        int[][] dp = new int[N][M];
-        
-        for (int i = 0; i < N; i++){
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-        
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        
-        q.add(new int[] {0,0,1} );
-        dp[0][0] = 1;
-        
-        int[] dx = new int[]{0,0,-1,1};
-        int[] dy = new int[]{1,-1,0,0};
-        
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            
-            int x = now[0];
-            int y = now[1];
-            int dist = now[2];
-            
-            if (x == N-1 && y == M -1) {
-                ans = dp[x][y];
-                break;
-            } 
-            
-            for (int d = 0; d < 4; d++){
-                int nx = dx[d] + x;
-                int ny = dy[d] + y;
-                if(nx < 0 || nx >= N || ny < 0 || ny >= M || maps[nx][ny] == 0) continue;
-                if (dp[nx][ny] > dist + 1){
-                    dp[nx][ny] = dist + 1;
-                    q.add(new int[] {nx, ny, dist + 1});
-                }
+
+        int n = maps.length;
+        int m = maps[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for(int i=0;i<4;i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(maps[nx][ny] != 1) continue;
+
+                maps[nx][ny] = maps[x][y] + 1;
+                q.add(new int[]{nx,ny});
             }
         }
-        
-        return ans;
+
+        return maps[n-1][m-1] == 1 ? -1 : maps[n-1][m-1];
     }
 }

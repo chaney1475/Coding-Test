fdf1417ab2c03cda99c3f1f97e78c957c2cb44c1
chaney1475 <chaney2225@gmail.com>
2026-04-03 17:44:37 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,49 +1,37 @@
-import java.util.*;
-
+import java.util.*;    
 class Solution {
-    
-    int[][] compu;
-    boolean visited[];
+    int count = 0;
+    boolean[] visited;
     int n;
-    int ans;
-    
+    int[][] computers;
     public int solution(int n, int[][] computers) {
+        
+        count = 0;
+        visited = new boolean[n]; // 0부터 n-1까지의 
         this.n = n;
-        visited = new boolean[n];
-        compu = computers;
+        this.computers = computers;
         
-        ans = 0;
+        int answer = 0;
+        // 0번부터 n-1까지의 컴퓨터가 있음
+        // 싹 돌아서 네트워크를 읽기
         
-        for (int i = 0; i < n; i++){
-            if (!visited[i]) {
-                ans ++;
-                bfs(i);
-            }
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            count++;
+            visited[i] = true;
+            dfs(i); // 특정 컴퓨터와 연결된 모든 네트워크 방문하기
         }
         
-        return ans;
+        return count;
     }
-    void bfs(int x){
-        Queue<Integer> q = new LinkedList<>();
+    
+    public void dfs(int node) {
         
-        visited[x] = true;
-        q.add(x);
-        
-        System.out.println("시작 : " + x);
-        
-        while(!q.isEmpty()){
-            
-            int now = q.poll();
-            
-            for (int i = 0; i < n; i++){
-                if (compu[now][i] == 1 && !visited[i]){
-                    q.add(i);
-                    visited[i] = true;
-                }
-            }
-            
+        for (int i = 0; i < n; i++) {
+            if (visited[i] || computers[node][i] == 0) continue;
+            visited[i] = true;
+            dfs(i);
         }
-        System.out.println(Arrays.toString(visited));
-        
     }
+    
 }

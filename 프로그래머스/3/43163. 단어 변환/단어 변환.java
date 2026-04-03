72bad737f0158c2e559e7141147a993e3c05f21a
chaney1475 <chaney2225@gmail.com>
2026-04-03 20:27:59 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,64 +1,55 @@
-/*
-각 위치에서 변할 수 있는 단어들이 정해져있음
-비교를 해서 한개 차이가 나면 거기로 갈 수 있도록?
-나왔던 단어는 방문 불가능
-dfs로?
-
-*/
+import java.util.*;
 
 class Solution {
-    static int len; // 단어의 길이
-    static int N; // 단어 갯수
-    
-    static boolean[] temp;
-    
-    static int ans;
-    static String[] word;
-    static String tar;
-    
     public int solution(String begin, String target, String[] words) {
-        len = words[0].length();
-        word = words;
         
-        ans = Integer.MAX_VALUE;
-        N = words.length;
-        tar = target;
+        int answer = 0;
         
-        temp = new boolean[N];
+        int n = words.length;
+        int[] dist = new int[n];
         
-        dfs(begin, 0);
+        Arrays.fill(dist, -1);
         
-        return (ans == Integer.MAX_VALUE) ? 0 : ans;
-    }
-    
-    static void dfs(String now, int cnt){
+        Queue<Integer> q = new LinkedList<>();
         
-        if (cnt > ans) return;
-        
-        if (now.equals(tar)) {
-            ans = Math.min(ans, cnt);
-            return;
-        }
-        
-        for (int i = 0; i < N; i++){
-            if (canChange(now, word[i]) && !temp[i]) {
-                temp[i] = true;
-                dfs(word[i], cnt + 1);
-                temp[i] = false;
+        int e = -1; // 타겟의 인덱스
+        for (int i = 0; i < words.length; i++) {
+            
+            if (words[i].equals(target)) {
+                e = i;
+            }
+                
+            if (check(begin, words[i])) {
+                q.add(i);
+                dist[i] = 1;
             }
         }
+        if (e == -1) return 0;
+        
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            
+            if (node == e) break;
+            for (int i = 0; i < n; i++) {
+                if (check(words[i], words[node]) && dist[i] == -1) {
+                    dist[i] = dist[node] + 1;
+                    q.add(i);
+                } 
+            }
+        }
+        
+        if (dist[e] == -1) return 0;
+        return dist[e];
     }
     
-    static boolean canChange(String a, String b){
-        
+    public boolean check(String a, String b) {
         int cnt = 0;
-        for (int i = 0; i < len; i++){
-            if (a.charAt(i) != b.charAt(i)){
-                cnt++;
-                if (cnt > 2) return false;
-            }
+        
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == b.charAt(i)) cnt++;
         }
-        if (cnt == 1) return true;
+        
+        if (cnt == a.length() - 1) return true;
         else return false;
     }
 }

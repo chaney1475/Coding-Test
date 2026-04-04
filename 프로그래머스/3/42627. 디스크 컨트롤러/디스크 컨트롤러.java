2c11f09bc1506a5a4a271623d7d4f3a24648a7f8
chaney1475 <chaney2225@gmail.com>
2026-04-04 20:39:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.80 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,74 +1,45 @@
 import java.util.*;
 
 class Solution {
-    
-    static class Job{
-        int start, end, duration;
-        Job(int start, int end, int duration){
-            this.start = start; this.end = end; this.duration = duration;
-        }
-        // @Override
-        // public String toString(){
-        //     return start + " " + end + " | ";
-        // }
-    }
-    
     public int solution(int[][] jobs) {
         
-        List<Job> list = new ArrayList<>();
-        
         int N = jobs.length;
-        
-        int time = 0;
-        
-        for (int i = 0; i < N ; i++){
-            int[] now = jobs[i];
-            list.add(new Job(now[0], now[0] + now[1], now[1]));
-        }
-        
-        Collections.sort(list, (a,b) -> {
-            if (a.start != b.start){
-                return a.start - b.start;
-            }else{
-                return a.end - b.end;
-            }
-        });
-        
-        PriorityQueue<Job> q = new PriorityQueue<>((a,b) -> a.duration - b.duration);
-        
-        int idx = 0;
-        
         int sum = 0;
         
-        while (idx < N){
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        
+        PriorityQueue<int[]> q = new PriorityQueue<>(
+            (a, b) -> {
+                if (a[0] != b[0]) return a[0] - b[0]; // 소요시간
+                if (a[1] != b[1]) return a[1] - b[1]; // 요청시간
+                return a[2] - b[2]; // 번호
+            }
+        );
+        
+        int time = 0;
+        int i = 0;
+        
+        while (i < N || !q.isEmpty()) {
             
-            //현재 시간이전에 들어온 애들은 큐에 넣어준다
-            
-            while (idx < N && list.get(idx).start <= time){
-                q.add(list.get(idx++));
+            // 현재 시간까지 들어온 작업 큐에 넣기
+            while (i < N && jobs[i][0] <= time) {
+                q.add(new int[]{jobs[i][1], jobs[i][0], i});
+                i++;
             }
             
-            //없는 경우에는 현재 봐야하는 거에서 꺼내기!
-            if(q.isEmpty()){
-                Job now = list.get(idx++);
-                time = now.end;
-                sum += (time - now.start);
-            }else{
-                //큐에 있는 경우에는 맨 앞 뽑기
-                Job now = q.poll();
-                //현재 시간에다가 이 작업의 걸리는 시간 더하기
-                time += now.duration;
-                sum += (time - now.start);
+            if (!q.isEmpty()) {
+                int[] cur = q.poll();
+                
+                int duration = cur[0];
+                int request = cur[1];
+                
+                time += duration;
+                sum += time - request;
+                
+            } else {
+                time = jobs[i][0];
             }
         }
-        
-        while(!q.isEmpty()){
-            Job now = q.poll();
-            time += now.duration;
-            sum += (time - now.start);
-        }
-        
-        System.out.println(sum);
         
         return sum / N;
     }

62249c5fcee7b8f00ecf1ed7523d6e75ef9c91c4
chaney1475 <chaney2225@gmail.com>
2026-04-16 20:26:23 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 13.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,27 +1,5 @@
 import java.util.*;
 
-/*
-올라갈 수 잇는 수 : length
-견딜 수 잇는 무게 : weight
-
-트럭 배열 [7,4,5,6]
-
-1초씩 체크 하기
-
-수랑 무게 체크 : 올릴 수 잇다?
-올리기
-
-못 올린다 하면
-맨 앞에잇는애가 언제나가는지 보기
-
-올릴때 나가는 수 적기
-1초에 올렷으면 언제 나갈 수 잇는지
-무게랑 나갈 초수 같이 쓰기
-1에 올라온 애면 3에 나감
-올라온 시간 + 길이
-
-*/
-
 class Solution {
     public int solution(int length, int weight, int[] truck) {
         

ae478a5c59f95881caceccdc9813c02bd508b751
chaney1475 <chaney2225@gmail.com>
2026-05-07 12:55:51 +0900

    [level 1] Title: 둘만의 암호, Time: 0.53 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -2,43 +2,28 @@ import java.util.*;
 
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        
-        char[] ans = new char[] {'a','b','c','d','e','f','g','h','i'
-            ,'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        
-        List<Integer> alpha = new ArrayList<>();
-        for (int i = 0; i < 26; i++){
-            alpha.add(i);
+        int N = s.length();
+        StringBuilder ans = new StringBuilder();
+
+        Set<Integer> skips = new HashSet<>();
+
+        for (int i = 0; i < skip.length(); i++) {
+            skips.add(skip.charAt(i) - 'a'); // 숫자만 저장
         }
-        
-        for (int i = 0; i < skip.length(); i++){
-            alpha.remove(new Integer(skip.charAt(i) - 'a'));
-        }
-        
-        for (Integer i : alpha){
-            System.out.print(ans[i] + " ");
-        }
-        
-        //index만큼 더한다
-        
-        StringBuilder sb = new StringBuilder();
-        int len = alpha.size();
-        
-        //사이즈만큼 돌아감!
-        for (int i = 0; i < s.length(); i++){
+
+        for (int i = 0; i < N; i++) {
+            char cur = s.charAt(i); // 현재 알파벳
             
-            int now = alpha.indexOf(s.charAt(i) - 'a');
-            // System.out.println("now : " + now);
-            int next = (now + index) % len;
+            int tmp = cur - 'a'; // 현재 숫자로 표현된 알파벳
             
-            // System.out.println("next : " + next);
-            
-            sb.append(ans[alpha.get(next)]); //인덱스 값 가져오기
+            for (int j = 0; j < index; j++) {
+                tmp = (tmp + 1) % 26;
+                if (skips.contains(tmp)) {
+                    j--; // 한 번 더 가게
+                }
+            }
+            ans.append((char)(tmp + 'a'));
         }
-        
-        System.out.println(sb);
-        
-        return sb.toString();
+        return ans.toString();
     }
 }

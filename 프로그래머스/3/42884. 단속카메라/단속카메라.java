0c1cf3acfe67b7659a57923f308ae348cbfe8612
chaney1475 <chaney2225@gmail.com>
2026-06-09 12:42:03 +0900

    [level 3] Title: 단속카메라, Time: 5.77 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,32 +1,36 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (a, b) -> a[0] - b[0]);
+        int answer = 0;
+        Arrays.sort(routes, (a,b) -> {
+           if (a[0] != b[0]) {
+               return a[0] - b[0];
+           }
+            return a[1] - b[1];
+        });
         
-        int N = routes.length;
-        
-        int s = +30001;
+        int s = -30001;
         int e = -30001;
+        int ans = 0;
         
-        int cnt = 0; // 일단 한 개도 없음
+        int n = routes.length;
         
-        for (int i = 0; i < N; i++) {
-            
-            int[] nr = routes[i];
-            int newS = nr[0];
-            int newE = nr[1];
+        for (int i = 0; i < n; i++) {
+            int x = routes[i][0];
+            int y = routes[i][1];
                 
-            if (e >= newS) {
-                s = Math.max(newS , s);
-                e = Math.min(newE , e);
-            } else {
-                cnt ++;
-                s = newS;
-                e = newE;
+            if (e < x) {
+                ans++;
+                
+                s = x;
+                e = y;
+            } else {     
+                s = Math.max(s, x);
+                e = Math.min(e, y);
             }
+
         }
         
-        return cnt;
+        return ans;
     }
 }

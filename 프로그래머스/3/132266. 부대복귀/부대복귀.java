a3b7489d818022f114f0742355369d2433399a6e
chaney1475 <chaney2225@gmail.com>
2026-06-11 13:06:16 +0900

    [level 3] Title: 부대복귀, Time: 88.80 ms, Memory: 206 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,52 +1,45 @@
 import java.util.*;
 
 class Solution {
-    
-    public int[] solution(int n, int[][] roads, int[] sources, int dest) {
-        int N = n;
-        int[] ans = new int[sources.length];
-        
-        int len = roads.length; // 로드의 길이
-        
-        ArrayList<Integer>[] adj = new ArrayList[N+1];
-        for (int i = 0; i <= N; i++){
-            adj[i] = new ArrayList<Integer>();
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        List<Integer>[] graph = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        
-        for (int i = 0; i < len; i++){
-            int a = roads[i][0];
-            int b = roads[i][1];
-            adj[a].add(b);
-            adj[b].add(a);
+
+        for (int[] road : roads) {
+            int a = road[0];
+            int b = road[1];
+
+            graph[a].add(b);
+            graph[b].add(a);
         }
-        int dist[] = new int[N+1];
+
+        int[] dist = new int[n + 1];
         Arrays.fill(dist, -1);
-        
-        dist[dest] = 0;
-        
-        // 숫자가 더 작은게 앞으로
+
         Queue<Integer> q = new LinkedList<>();
-        q.add(dest);
-        
-        while(!q.isEmpty()){
-            int now = q.poll();
-            for (Integer next : adj[now]){
-                if(dist[next] == -1){
-                    dist[next] = dist[now] + 1;
+        dist[destination] = 0;
+        q.add(destination);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int next : graph[cur]) {
+                if (dist[next] == -1) {
+                    dist[next] = dist[cur] + 1;
                     q.add(next);
                 }
             }
         }
-        
-        for (int i = 0; i < sources.length; i++){
-            int now = dist[sources[i]];
-            if (now == Integer.MAX_VALUE){
-                ans[i] = -1;
-            }else{
-                ans[i] = now;
-            }
+
+        int[] ans = new int[sources.length];
+
+        for (int i = 0; i < sources.length; i++) {
+            ans[i] = dist[sources[i]];
         }
-        
+
         return ans;
     }
 }
